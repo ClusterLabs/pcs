77efdf360d5f57997cef706f82d87b85c65c9e53
Chris Feist <cfeist@redhat.com>
2011-12-12 15:09:44 -0600

    Fixed file support to deal with empty files.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os, subprocess
+import sys
 import ccs
 
 # usefile & filename variables are set in ccs module
@@ -11,6 +12,34 @@ def run(args):
     if usefile:
         env_var["CIB_file"] = filename
 
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, env = env_var)
-    output = p.stdout.read()
+    if not os.path.isfile(filename):
+        try:
+            write_empty_cib(filename)
+        except IOError:
+            print "Unable to write to file: " + filename
+            sys.exit(1)
+
+    try:
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, env = env_var)
+        output = p.stdout.read()
+    except OSError:
+        print "Unable to locate command: " + args[0]
+        sys.exit(1)
+
     return output
+
+def write_empty_cib(filename):
+
+    empty_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<cib admin_epoch="0" epoch="1" num_updates="1" validate-with="pacemaker-1.2">
+  <configuration>
+    <crm_config/>
+    <nodes/>
+    <resources/>
+    <constraints/>
+  </configuration>
+  <status/>
+</cib>"""
+    f = open(filename, 'w')
+    f.write(empty_xml)
+    f.close()
