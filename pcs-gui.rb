836ce53e2c590e6532e98af9aa1cfba3cd6976c4
Chris Feist <cfeist@redhat.com>
2012-02-21 23:39:22 -0600

    Set default cursor over nodes, node information updates.

--- pcs-gui.rb
+++ pcs-gui.rb
@@ -15,9 +15,18 @@ get '/nodes/?' do
 end
 
 get '/nodes/:node' do
-  @nodes = (1..7)
+  @nodes = {} 
+  (1..7).each do |i|
+    @nodes[i] = [i, "Node #{i}", "node#{i}.lab.msp.redhat.com"]
+  end
   @cur_node = params[:node].to_i
-  @cur_node = 1 if @cur_node == 0
+  if @cur_node == 0 then
+    @cur_node = @nodes[1]
+  else
+    @cur_node = @nodes[@cur_node]
+  end
+  print "TEST"
+  print @nodes
   erb :index
 end
 
