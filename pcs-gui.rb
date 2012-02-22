fee7752ab61214ba46942aa166fb989f880a2b3a
Chris Feist <cfeist@redhat.com>
2012-02-21 23:21:34 -0600

    Added support for picking nodes & fixed hostname display

--- pcs-gui.rb
+++ pcs-gui.rb
@@ -1,9 +1,28 @@
 require 'sinatra'
 
+@nodes = (1..7)
+  
 get '/' do
+  @nodes = (1..7)
+  @cur_node = 1
   erb :index
 end
 
-get '/nodes' do
+get '/nodes/?' do
+  @nodes = (1..7)
+  @cur_node = 1
+  erb :index
+end
+
+get '/nodes/:node' do
+  @nodes = (1..7)
+  @cur_node = params[:node].to_i
+  @cur_node = 1 if @cur_node == 0
+  erb :index
+end
+
+get '*' do
+  @cur_node = 1
+  @nodes = [1,2]
   erb :index
 end
