32b157821a6311cec479b7f68db30bbb85bf0b47
Chris Feist <cfeist@redhat.com>
2012-02-23 15:08:14 -0600

    Added resource page and updated live cluster views of nodes/resources.

--- pcs-gui.rb
+++ pcs-gui.rb
@@ -1,22 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'open3'
+require 'rexml/document'
 
 #set :port, 2222
 set :logging, true
+
 
 @nodes = (1..7)
 
 helpers do
   def setup
-    @nodes = {} 
-    (1..7).each do |i|
-      @nodes[i] = [i, "Node #{i}", "node#{i}.lab.msp.redhat.com"]
+    @nodes_online, @nodes_offline = getNodes
+    @nodes = {}
+    @nodes_online.each do |i|
+      @nodes[i]  = Node.new(i, i, i, true)
     end
-    @cur_node = params[:node].to_i
-    if @cur_node == 0 then
-      @cur_node = @nodes[1]
+    @nodes_offline.each do |i|
+      @nodes[i]  = Node.new(i, i, i, false)
+    end
+
+    if params[:node]
+      @cur_node = @nodes[params[:node]]
     else
-      @cur_node = @nodes[@cur_node]
+      @cur_node = @nodes.values[0]
     end
   end
 end
@@ -27,22 +34,35 @@ get '/blah' do
 end
 
 get '/resourcedeps/?:resource?' do
-  setup()
   @resourcedepsmenuclass = "class=\"active\""
-  erb :index
+  erb :resourcedeps, :layout => :main
 end
 
 get '/resources/?:resource?' do
-  setup()
+  @resources = getResources
   @resourcemenuclass = "class=\"active\""
-  erb :index
+
+  @cur_resource = @resources[0]
+  if params[:resource]
+    @resources.each do |r|
+      if r.id == params[:resource]
+	@cur_resource = r
+	break
+      end
+    end
+  end
+
+  puts "OUTPUT"
+  puts @cur_resource.id
+  puts @cur_resource.name
+  erb :resource, :layout => :main
 end
 
 get '/nodes/?:node?' do
   print "Nodes\n"
   setup()
   @nodemenuclass = "class=\"active\""
-  erb :index
+  erb :nodes, :layout => :main
 end
 
 get '/' do
@@ -55,4 +75,56 @@ get '*' do
   print params[:splat]
   print "2Redirecting...\n"
   call(env.merge("PATH_INFO" => '/nodes'))
+end
+
+def getNodes
+  stdin, stdout, stderror = Open3.popen3('/root/pcs/pcs/pcs status nodes')
+  out = stdout.readlines
+  [out[1].split(' ')[1..-1], out[2].split(' ')[1..-1]]
+end
+
+def getResources
+  stdin, stdout, stderror = Open3.popen3('/root/pacemaker/tools/crm_mon --as-xml=/tmp/testclusterstatus')
+
+  doc = REXML::Document.new(File.open("/tmp/testclusterstatus", "rb"))
+  resource_list = []
+  puts "PRE"
+  doc.elements.each('crm_mon/resources/resource') do |e|
+    resource_list.push(Resource.new(e))
+  end
+  resource_list
+end
+
+class Node
+  attr_accessor :active, :id, :name, :hostname
+
+  def initialize(id=nil, name=nil, hostname=nil, active=nil)
+    @id, @name, @hostname, @active = id, name, hostname, active
+  end
+end
+
+class Resource 
+  attr_accessor :id, :name, :type, :agent, :role, :active, :orphaned, :managed,
+    :failed, :failure_ignored, :nodes, :location
+  def initialize(e)
+    @id = e.attributes["id"]
+    @agent = e.attributes["resource_agent"]
+    @active = e.attributes["active"] == "true" ? true : false
+    @orphaned = e.attributes["orphaned"] == "true" ? true : false
+    @failed = e.attributes["failed"] == "true" ? true : false
+    @nodes = []
+    print "New Resource!\n"
+    e.elements.each do |n| 
+      print "NODE #{n.attributes["id"]}\n\n\n"
+      node = Node.new
+      node.name = n.attributes["name"]
+      node.id = n.attributes["id"]
+      @nodes.push(node)
+    end
+    if @nodes.length != 0
+      @location = @nodes[0].name
+    else
+      @location = ""
+    end
+  end
 end
