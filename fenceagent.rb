066a045b423a13f0583e4a874e1dd3aca59dab17
Chris Feist <cfeist@redhat.com>
2012-05-01 18:06:55 -0500

    Fix options when adding fence agents.

--- fenceagent.rb
+++ fenceagent.rb
@@ -1,15 +1,11 @@
 def getFenceAgents(fence_agent = nil)
   fence_agent_list = {}
-  if fence_agent == nil
-    return fence_agent_list
-  end
-
   agents = Dir.glob('/usr/sbin/fence_' + '*')
   agents.each { |a|
     fa = FenceAgent.new
     fa.name =  a.sub(/.*\//,"")
 
-    if a.sub(/.*\//,"") == fence_agent.sub(/.*:/,"")
+    if fence_agent and a.sub(/.*\//,"") == fence_agent.sub(/.*:/,"")
       required_options, optional_options = getFenceAgentMetadata(fa.name)
       fa.required_options = required_options
       fa.optional_options = optional_options
@@ -20,7 +16,11 @@ def getFenceAgents(fence_agent = nil)
 end
 
 def getFenceAgentMetadata(fenceagentname)
-  metadata = `stonith_admin --metadata -a #{fenceagentname}`
+  # There are bugs in stonith_admin & the new fence_agents interaction
+  # eventually we'll want to switch back to this, but for now we directly
+  # call the agent to get metadata
+  #metadata = `stonith_admin --metadata -a #{fenceagentname}`
+  metadata = `/usr/sbin/#{fenceagentname} -o metadata`
   doc = REXML::Document.new(metadata)
   options_required = {}
   options_optional = {}
