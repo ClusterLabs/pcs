ca912b55143f97f1febf808fb513c15e9e3bd282
Chris Feist <cfeist@redhat.com>
2012-05-07 14:47:10 -0500

    Updated setup.py tag to 0.9.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pcs',
-    version='0.9.3',
+    version='0.9.3.1',
     description='Pacemaker Configuration System',
     author='Chris Feist',
     author_email='cfeist@redhat.com',
