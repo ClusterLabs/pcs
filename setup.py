c658236541661d950fd83f3a6f173dbdcef3a238
Chris Feist <cfeist@redhat.com>
2012-06-12 14:38:39 -0500

    Marked setup.py for 0.9.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pcs',
-    version='0.9.4',
+    version='0.9.5',
     description='Pacemaker Configuration System',
     author='Chris Feist',
     author_email='cfeist@redhat.com',
