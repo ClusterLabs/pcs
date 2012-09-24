6438216cfa1995ef6d29a11bf0e7e92c8345a9cb
Chris Feist <cfeist@redhat.com>
2012-09-24 13:04:32 -0500

    Fixed make install & bumped to 0.9.25

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ endif
 install:
 	python setup.py install --prefix ${DESTDIR}/usr
 	mkdir -p ${DESTDIR}/usr/sbin/
+	chmod 755 ${DESTDIR}/${PYTHON_SITELIB}/pcs/pcs.py
 	ln -s ${PYTHON_SITELIB}/pcs/pcs.py ${DESTDIR}/usr/sbin/pcs
 
 tarball:
--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@
 from distutils.core import setup
 
 setup(name='pcs',
-    version='0.9.24',
+    version='0.9.25',
     description='Pacemaker Configuration System',
     author='Chris Feist',
     author_email='cfeist@redhat.com',
     url='http://github.com/feist/pcs',
     packages=['pcs'],
     package_data={'pcs':['corosync.conf.template','corosync.conf.fedora.template']}, 
-    py_modules=['pcs'],
-    scripts=['pcs/pcs.py']
+    py_modules=['pcs']
     )
