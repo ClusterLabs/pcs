315492458bb3e101ad2b922410d5d848d02c60b4
Chris Feist <cfeist@redhat.com>
2012-10-17 15:05:47 -0500

    Set defaults to use /sbin/ instead of /usr/sbin

--- pcs/settings.py
+++ pcs/settings.py
@@ -1,4 +1,4 @@
-pacemaker_binaries = "/usr/sbin/"
+pacemaker_binaries = "/sbin/"
 corosync_binaries = "/sbin/"
 corosync_conf_file = "/etc/corosync/corosync.conf"
-fence_agent_binaries = "/usr/sbin/"
+fence_agent_binaries = "/sbin/"
