471195935ed85b570bf66cc3d32d3df158451299
Chris Feist <cfeist@redhat.com>
2013-01-02 14:33:33 -0600

    Updated certificate generation and signing for firefox

--- ssl.rb
+++ ssl.rb
@@ -7,19 +7,21 @@ require 'rack'
 CRT_FILE = "/var/lib/pcsd/pcsd.crt"
 KEY_FILE = "/var/lib/pcsd/pcsd.key"
 server_name = WEBrick::Utils::getservername
-name = "/C=US/ST=MN/L=Minneapolis/O=pcsd/OU=pcsd/CN=#{server_name}"
-ca   = OpenSSL::X509::Name.parse(name)
-key = OpenSSL::PKey::RSA.new(1024)
-crt = OpenSSL::X509::Certificate.new
-crt.version = 2
-crt.serial  = 4
-crt.subject = ca
-crt.issuer = ca
-crt.public_key = key.public_key
-crt.not_before = Time.now
-crt.not_after  = Time.now + 10 * 365 * 24 * 60 * 60 # 10 year
 
 if not File.exists?(CRT_FILE) or not File.exists?(KEY_FILE)
+  name = "/C=US/ST=MN/L=Minneapolis/O=pcsd/OU=pcsd/CN=#{server_name}"
+  ca   = OpenSSL::X509::Name.parse(name)
+  key = OpenSSL::PKey::RSA.new(2048)
+  crt = OpenSSL::X509::Certificate.new
+  crt.version = 2
+  crt.serial  = ((Time.now).to_f * 1000).to_i
+  crt.subject = ca
+  crt.issuer = ca
+  crt.public_key = key.public_key
+  crt.not_before = Time.now
+  crt.not_after  = Time.now + 10 * 365 * 24 * 60 * 60 # 10 year
+  crt.sign(key, OpenSSL::Digest::SHA256.new)
+
   File.open(CRT_FILE, 'w') {|f| f.write(crt)}
   File.open(KEY_FILE, 'w') {|f| f.write(key)}
 end
