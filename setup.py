d5fb2106902eca15c31e59bf1a4ecccfd9480ede
Chris Feist <cfeist@redhat.com>
2013-01-14 15:21:25 -0600

    Bumped version to 0.9.30 and added pcsd to tarball

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pcs',
-    version='0.9.29',
+    version='0.9.30',
     description='Pacemaker Configuration System',
     author='Chris Feist',
     author_email='cfeist@redhat.com',
