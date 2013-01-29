ce8a0d0b8afe86f64a721418e0a245aeae504cb8
Chris Feist <cfeist@redhat.com>
2013-01-28 18:21:34 -0600

    Added check for proper 'pcs config' ouput

--- pcs/test/resource_create.py
+++ pcs/test/resource_create.py
@@ -12,10 +12,59 @@ temp_cib = "temp.xml"
 class ResourceAdditionTest(unittest.TestCase):
     def setUp(self):
         shutil.copy(empty_cib, temp_cib)
+
+    # Setups up a cluster with Resources, groups, master/slave resource & clones
+    def setupClusterA(self,temp_cib):
+        line = "resource create ClusterIP ocf:heartbeat:IPaddr2 ip=192.168.0.99 cidr_netmask=32 op monitor interval=30s"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+        line = "resource create ClusterIP2 ocf:heartbeat:IPaddr2 ip=192.168.0.99 cidr_netmask=32 op monitor interval=30s"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+        line = "resource create ClusterIP3 ocf:heartbeat:IPaddr2 ip=192.168.0.99 cidr_netmask=32 op monitor interval=30s"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+        line = "resource create ClusterIP4  ocf:heartbeat:IPaddr2 ip=192.168.0.99 cidr_netmask=32 op monitor interval=30s"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+        line = "resource create ClusterIP5 ocf:heartbeat:IPaddr2 ip=192.168.0.99 cidr_netmask=32 op monitor interval=30s"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+        line = "resource create ClusterIP6  ocf:heartbeat:IPaddr2 ip=192.168.0.99 cidr_netmask=32 op monitor interval=30s"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+
+        line = "resource group add TestGroup1 ClusterIP"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+
+        line = "resource group add TestGroup2 ClusterIP2 ClusterIP3"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+
+        line = "resource clone ClusterIP4"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+
+        line = "resource master Master ClusterIP5"
+        output, returnVal = pcs(temp_cib, line)
+        assert returnVal == 0
+        assert output == ""
+
     def testEmpty(self):
         output, returnVal = pcs(temp_cib, "resource") 
         assert returnVal == 0, 'Unable to list resources'
         assert output == "NO resources configured\n", "Bad output"
+
     def testAddResources(self):
         line = "resource create ClusterIP ocf:heartbeat:IPaddr2 ip=192.168.0.99 cidr_netmask=32 op monitor interval=30s"
         output, returnVal = pcs(temp_cib, line) 
@@ -146,6 +195,13 @@ class ResourceAdditionTest(unittest.TestCase):
         output, returnVal = pcs(temp_cib, "resource show ClusterIP")
         assert returnVal == 0
         assert output == 'Resource: ClusterIP\n  ip: 192.168.0.99\n  cidr_netmask: 32\n'
+
+    def testClusterConfig(self):
+        self.setupClusterA(temp_cib)
+
+        output, returnVal = pcs(temp_cib, "config")
+        assert returnVal == 0
+        assert output == 'Cluster Name: test99\nCorosync Nodes:\n rh7-1 rh7-2 \nPacemaker Nodes:\n \n\nResources: \n Resource: ClusterIP6 (type=IPaddr2 class=ocf provider=heartbeat)\n  Attributes: ip=192.168.0.99 cidr_netmask=32 \n  Operations: monitor interval=30s\n Group: TestGroup1\n  Resource: ClusterIP (type=IPaddr2 class=ocf provider=heartbeat)\n   Attributes: ip=192.168.0.99 cidr_netmask=32 \n   Operations: monitor interval=30s\n Group: TestGroup2\n  Resource: ClusterIP2 (type=IPaddr2 class=ocf provider=heartbeat)\n   Attributes: ip=192.168.0.99 cidr_netmask=32 \n   Operations: monitor interval=30s\n  Resource: ClusterIP3 (type=IPaddr2 class=ocf provider=heartbeat)\n   Attributes: ip=192.168.0.99 cidr_netmask=32 \n   Operations: monitor interval=30s\n Clone: ClusterIP4-clone\n  Resource: ClusterIP4 (type=IPaddr2 class=ocf provider=heartbeat)\n   Attributes: ip=192.168.0.99 cidr_netmask=32 \n   Operations: monitor interval=30s\n Master: Master\n  Resource: ClusterIP5 (type=IPaddr2 class=ocf provider=heartbeat)\n   Attributes: ip=192.168.0.99 cidr_netmask=32 \n   Operations: monitor interval=30s\n\nLocation Constraints:\nOrdering Constraints:\nColocation Constraints:\n\nCluster Properties:\n', output
 
 # Run pcs with -f on specified file
 def pcs(testfile, args):
