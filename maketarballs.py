93057de92c3edd48acf0021b9b9abe48de94356b
Chris Feist <cfeist@redhat.com>
2013-11-06 18:15:42 -0600

    Start using gemfiles

--- maketarballs.py
+++ maketarballs.py
@@ -8,6 +8,6 @@ import settings
 pcs_version = settings.pcs_version
 
 print os.system("cp dist/pcs-"+pcs_version+".tar dist/pcs-withgems-"+pcs_version+".tar")
-print os.system("tar --delete -f dist/pcs-"+pcs_version+".tar '*/pcsd/gems'")
+print os.system("tar --delete -f dist/pcs-"+pcs_version+".tar '*/pcsd/vendor'")
 print os.system("gzip dist/pcs-"+pcs_version+".tar")
 print os.system("gzip dist/pcs-withgems-"+pcs_version+".tar")
