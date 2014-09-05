f46506323de2a6b374bc5e9b079a05bd6ea1e52c
Chris Feist <cfeist@redhat.com>
2014-09-05 17:24:34 -0500

    Bumped to 0.9.125

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pcs',
-    version='0.9.124',
+    version='0.9.125',
     description='Pacemaker Configuration System',
     author='Chris Feist',
     author_email='cfeist@redhat.com',
