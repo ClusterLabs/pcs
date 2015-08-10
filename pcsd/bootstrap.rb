15af89908e1b662c29e58a1ba806f29aadba71e2
Tomas Jelinek <tojeline@redhat.com>
2015-08-10 10:58:56 +0200

    Bumped to 0.9.143

--- pcsd/bootstrap.rb
+++ pcsd/bootstrap.rb
@@ -39,7 +39,7 @@ def get_pcs_path(pcsd_path)
   end
 end
 
-PCS_VERSION = '0.9.142'
+PCS_VERSION = '0.9.143'
 ISRHEL6 = is_rhel6
 ISSYSTEMCTL = is_systemctl
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pcs',
-    version='0.9.142',
+    version='0.9.143',
     description='Pacemaker Configuration System',
     author='Chris Feist',
     author_email='cfeist@redhat.com',
