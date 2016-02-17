24f3c946967cfec05be7e0bd934a2dba273567fc
Tomas Jelinek <tojeline@redhat.com>
2016-02-17 12:29:24 +0100

    Bumped to 0.9.149

--- pcsd/bootstrap.rb
+++ pcsd/bootstrap.rb
@@ -43,7 +43,7 @@ def get_pcs_path(pcsd_path)
   end
 end
 
-PCS_VERSION = '0.9.148'
+PCS_VERSION = '0.9.149'
 COROSYNC = COROSYNC_BINARIES + "corosync"
 ISRHEL6 = is_rhel6
 ISSYSTEMCTL = is_systemctl
