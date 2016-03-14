2f1faf4d970793a267c15284bc99b588de8f4944
Ivan Devat <idevat@redhat.com>
2016-03-14 18:58:55 +0100

    clean thoroughly after installation

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import os
+
+from setuptools import setup, Command, find_packages
+
+class CleanCommand(Command):
+    user_options = []
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+        os.system('rm -rf ./build ./dist ./*.pyc ./*.egg-info')
 
 setup(
     name='pcs',
@@ -16,4 +28,7 @@ setup(
             'pcs = pcs.cli:main',
         ],
     },
+    cmdclass={
+        'clean': CleanCommand,
+    }
 )
