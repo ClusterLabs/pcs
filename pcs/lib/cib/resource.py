e7d12da3fbe59c0449bd4d3c4fa92606629c29f6
Ivan Devat <idevat@redhat.com>
2016-06-13 15:35:04 +0200

    expect resource id before real resource tag in cib

--- pcs/lib/cib/resource.py
+++ pcs/lib/cib/resource.py
@@ -9,7 +9,7 @@ TAGS_CLONE = "clone", "master"
 TAGS_ALL = TAGS_CLONE + ("primitive", "group")
 
 def find_by_id(tree, id):
-    element = tree.find('.//*[@id="{0}"]'.format(id))
-    if element is None or element.tag not in TAGS_ALL:
-        return None
-    return element
+    for element in tree.findall('.//*[@id="{0}"]'.format(id)):
+        if element is not None and element.tag in TAGS_ALL:
+            return element
+    return None
---
+++ pcs/lib/cib/test/test_resource.py
@@ -0,0 +1,21 @@
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+from unittest import TestCase
+from lxml import etree
+from pcs.lib.cib.resource import find_by_id
+
+class FindByIdTest(TestCase):
+    def test_find_correct_tag(self):
+        tree = etree.XML("""
+            <root>
+                <rsc_set id="A" />
+                <primitive id="A" />
+            </root>
+        """)
+        element = find_by_id(tree, "A")
+        self.assertEqual(element.tag, "primitive")
