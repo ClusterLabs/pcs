fe63178a8eeb15d0d1f2310f8cf8509d55bf3fff
Ivan Devat <idevat@redhat.com>
2017-05-22 09:06:44 +0200

    remove unused pcsd service manage code

--- pcsd/pcsd_action_command.rb
+++ pcsd/pcsd_action_command.rb
@@ -86,23 +86,7 @@ module PcsdActionCommand
     end
   end
 
-  class RemovePcmkRemoteAuthkey < ActionType
-    def process()
-      authkey_file = File.join(PACEMAKER_CONFIG_DIR, "authkey")
-      unless File.exists? authkey_file
-        return PcsdExchangeFormat::result(:not_found)
-      end
-      begin
-        File.delete(authkey_file)
-        return PcsdExchangeFormat::result(:deleted)
-      rescue => e
-        return PcsdExchangeFormat::result(:unexpected, e.message)
-      end
-    end
-  end
-
   TYPES = {
     "service_command" => ServiceCommand,
-    "remove_pcmk_remote_authkey" => RemovePcmkRemoteAuthkey,
   }
 end
