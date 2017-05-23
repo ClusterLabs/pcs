606f10bacfaa010bf1a85d15cd003d1b51b8f16f
Ivan Devat <idevat@redhat.com>
2017-05-23 09:21:55 +0200

    allow `cluster node clear` only in live env

--- pcs/lib/commands/cluster.py
+++ pcs/lib/commands/cluster.py
@@ -460,6 +460,22 @@ def node_remove_guest(
     )
 
 def node_clear(env, node_name, allow_clear_cluster_node=False):
+    """
+    Remove specified node from various cluster caches.
+
+    LibraryEnvironment env provides all for communication with externals
+    string node_name
+    bool allow_clear_cluster_node -- flag allows to clear node even if it's
+        still in a cluster
+    """
+    mocked_envs = []
+    if not env.is_cib_live:
+        mocked_envs.append("CIB")
+    if not env.is_corosync_conf_live:
+        mocked_envs.append("COROSYNC_CONF")
+    if mocked_envs:
+        raise LibraryError(reports.live_environment_required(mocked_envs))
+
     current_nodes = get_nodes(env.get_corosync_conf(), env.get_cib())
     if(
         node_addresses_contain_name(current_nodes, node_name)
