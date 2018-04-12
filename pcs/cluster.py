4eb8c8f5163b0da238054ef6c307c7194a33a643
Bruno Travouillon <devel@travouillon.fr>
2018-04-12 19:58:06 +0200

    fix 'cib-push' exit code when the diff is empty
    pcs cib-push diff-against= should not consider an empty diff as an
    error. Instead, we notice the user and exit with 0.

--- pcs/cluster.py
+++ pcs/cluster.py
@@ -1360,9 +1360,10 @@ def cluster_push(argv):
         if error.strip():
             utils.err("unable to diff the CIBs:\n" + error)
         if not patch.strip():
-            utils.err(
+            print(
                 "The new CIB is the same as the original CIB, nothing to push."
             )
+            sys.exit(0)
 
         command = ["cibadmin", "--patch", "--xml-pipe"]
         output, error, retval = runner.run(command, patch)
