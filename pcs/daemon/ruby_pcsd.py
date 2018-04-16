37e7d364e7fc8ede57d215b5d42f93b561af48e9
Ivan Devat <idevat@redhat.com>
2018-04-16 15:10:12 +0200

    log fails between python and ruby

--- pcs/daemon/ruby_pcsd.py
+++ pcs/daemon/ruby_pcsd.py
@@ -2,30 +2,28 @@ import base64
 import json
 import os.path
 from collections import namedtuple
+from time import time as now
 
 from tornado.gen import Task, multi, convert_yielded
+from tornado.web import HTTPError
 from tornado.httputil import split_host_and_port, HTTPServerRequest
 from tornado.process import Subprocess
+
+from pcs.daemon import log
 
 
 SINATRA_GUI = "sinatra_gui"
 SINATRA_REMOTE = "sinatra_remote"
 SYNC_CONFIGS = "sync_configs"
 
-SinatraResult = namedtuple("SinatraResult", "headers, status, body")
-
-def json_to_sinatra_result(result_json) -> SinatraResult:
-    try:
-        result = json.loads(result_json)
-    except Exception as e:
-        #TODO log?
-        raise e
-
-    return SinatraResult(
-        result["headers"],
-        result["status"],
-        base64.b64decode(result["body"])
-    )
+class SinatraResult(namedtuple("SinatraResult", "headers, status, body")):
+    @classmethod
+    def from_response(cls, response):
+        return cls(
+            response["headers"],
+            response["status"],
+            base64.b64decode(response["body"])
+        )
 
 class Wrapper:
     def __init__(
@@ -39,44 +37,38 @@ class Wrapper:
         self.__ruby_executable = ruby_executable
         self.__debug = debug
 
-    def get_ruby_request(self, request_type):
-        return {
+    def get_sinatra_request(self, request: HTTPServerRequest):
+        host, port = split_host_and_port(request.host)
+        return {"env": {
+            "PATH_INFO": request.path,
+            "QUERY_STRING": request.query,
+            "REMOTE_ADDR": request.remote_ip,
+            "REMOTE_HOST": request.host,
+            "REQUEST_METHOD": request.method,
+            "REQUEST_URI": f"{request.protocol}://{request.host}{request.uri}",
+            "SCRIPT_NAME": "",
+            "SERVER_NAME": host,
+            "SERVER_PORT": port,
+            "SERVER_PROTOCOL": request.version,
+            "HTTP_HOST": request.host,
+            "HTTP_ACCEPT": "*/*",
+            "HTTP_COOKIE": ";".join([
+                v.OutputString() for v in request.cookies.values()
+            ]),
+            "HTTPS": "on" if request.protocol == "https" else "off",
+            "HTTP_VERSION": request.version,
+            "REQUEST_PATH": request.uri,
+            "rack.input": request.body.decode("utf8"),
+        }}
+
+    async def run_ruby(self, request_type, request=None):
+        request = request or {}
+        request.update({
             "type": request_type,
             "config": {
                 "log_location": self.__log_file_location,
             },
-        }
-
-    def get_sinatra_request(self, request_type, request: HTTPServerRequest):
-        sinatra_request = self.get_ruby_request(request_type)
-
-        host, port = split_host_and_port(request.host)
-        sinatra_request.update({
-            "env": {
-                "PATH_INFO": request.path,
-                "QUERY_STRING": request.query,
-                "REMOTE_ADDR": request.remote_ip,
-                "REMOTE_HOST": request.host,
-                "REQUEST_METHOD": request.method,
-                "REQUEST_URI": f"{request.protocol}://{request.host}{request.uri}",
-                "SCRIPT_NAME": "",
-                "SERVER_NAME": host,
-                "SERVER_PORT": port,
-                "SERVER_PROTOCOL": request.version,
-                "HTTP_HOST": request.host,
-                "HTTP_ACCEPT": "*/*",
-                "HTTP_COOKIE": ";".join([
-                    v.OutputString() for v in request.cookies.values()
-                ]),
-                "HTTPS": "on" if request.protocol == "https" else "off",
-                "HTTP_VERSION": request.version,
-                "REQUEST_PATH": request.uri,
-                "rack.input": request.body.decode("utf8"),
-            }
         })
-        return sinatra_request
-
-    async def run_ruby(self, request):
         pcsd_ruby = Subprocess(
             [
                 self.__ruby_executable, "-I",
@@ -92,18 +84,32 @@ class Wrapper:
             }
         )
 
-        await Task(pcsd_ruby.stdin.write, str.encode(json.dumps(request)))
+        request_json = json.dumps(request)
+        await Task(pcsd_ruby.stdin.write, str.encode(request_json))
         pcsd_ruby.stdin.close()
-        result_json, dummy_error = await multi([
+        stdout, stderr = await multi([
             Task(pcsd_ruby.stdout.read_until_close),
             Task(pcsd_ruby.stderr.read_until_close),
         ])
-        return result_json
+
+        try:
+            return json.loads(stdout)
+        except Exception as e:
+            message_list = [f"Cannot decode json from ruby pcsd wrapper: '{e}'"]
+            if self.__debug:
+                message_list.extend([
+                    f"Request for ruby pcsd wrapper: '{request_json}'",
+                    f"Response stdout from ruby pcsd wrapper: '{stdout}'",
+                    f"Response stderr from ruby pcsd wrapper: '{stderr}'",
+                ])
+            for message in message_list:
+                log.pcsd.error(message)
+            raise HTTPError(500)
 
     async def request_gui(
         self, request: HTTPServerRequest, user, groups, is_authenticated
     ) -> SinatraResult:
-        sinatra_request = self.get_sinatra_request(SINATRA_GUI, request)
+        sinatra_request = self.get_sinatra_request(request)
         # Session was taken from ruby. However, some session information is needed
         # for ruby code (e.g. rendering some parts of templates). So this
         # information must be sent to ruby by another way.
@@ -112,18 +118,25 @@ class Wrapper:
                 "username": user,
                 "groups": groups,
                 "is_authenticated": is_authenticated,
-            },
+            }
         })
-        result_json = await convert_yielded(self.run_ruby(sinatra_request))
-        return json_to_sinatra_result(result_json)
+        response = await convert_yielded(self.run_ruby(
+            SINATRA_GUI,
+            sinatra_request
+        ))
+        return SinatraResult.from_response(response)
 
     async def request_remote(self, request: HTTPServerRequest) -> SinatraResult:
-        sinatra_request = self.get_sinatra_request(SINATRA_REMOTE, request)
-        result_json = await convert_yielded(self.run_ruby(sinatra_request))
-        return json_to_sinatra_result(result_json)
+        response = await convert_yielded(self.run_ruby(
+            SINATRA_REMOTE,
+            self.get_sinatra_request(request)
+        ))
+        return SinatraResult.from_response(response)
 
     async def sync_configs(self):
-        result_json = await convert_yielded(
-            self.run_ruby(self.get_ruby_request(SYNC_CONFIGS))
-        )
-        return json.loads(result_json)["next"]
+        try:
+            response = await convert_yielded(self.run_ruby(SYNC_CONFIGS))
+            return response["next"]
+        except HTTPError:
+            log.pcsd.error("Config synchronization failed")
+            return int(now()) + 5
