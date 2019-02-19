6eae0b0c51c286e3d812569208ea22a3c9c236a4
Tomas Jelinek <tojeline@redhat.com>
2019-02-19 18:01:25 +0100

    disable config files syncing for 1-node clusters
    In 1-node clusters, there are no nodes to sync with.

--- pcsd/pcsd.rb
+++ pcsd/pcsd.rb
@@ -85,7 +85,7 @@ def run_cfgsync
       # do not sync if this host is not in a cluster
       cluster_name = get_cluster_name()
       cluster_nodes = get_corosync_nodes_names()
-      if cluster_name and !cluster_name.empty?() and cluster_nodes and !cluster_nodes.empty?
+      if cluster_name and !cluster_name.empty?() and cluster_nodes and cluster_nodes.count > 1
         $logger.debug('Config files sync fetching')
         fetcher = Cfgsync::ConfigFetcher.new(
           PCSAuth.getSuperuserAuth(),
@@ -100,7 +100,8 @@ def run_cfgsync
         $logger.info('Config files sync finished')
       else
         $logger.info(
-          'Config files sync skipped, this host does not seem to be in a cluster'
+          'Config files sync skipped, this host does not seem to be in ' +
+          'a cluster of at least 2 nodes'
         )
       end
     rescue => e
