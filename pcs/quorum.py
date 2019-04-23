ClusterLabs/pcs

commit 61d7feb018db5dd21165b05c7f64ab85720cf0f6

check if --options are allowed for each command
* also fixes printing usage - print as specific usage as possible
* also fixes several errors in commandline parsing, mostly silently
  accepting not valid commands

pcs/quorum.py
@@ -1,5 +1,3 @@
-import sys
-
 from pcs import (
     stonith,
     usage,
@@ -29,7 +27,7 @@ def quorum_cmd(lib, argv, modifiers):
             quorum_device_cmd(lib, argv_next, modifiers)
         elif sub_cmd == "unblock":
             # TODO switch to new architecture
-            quorum_unblock_cmd(argv_next)
+            quorum_unblock_cmd(lib, argv_next, modifiers)
         elif sub_cmd == "update":
             quorum_update_cmd(lib, argv_next, modifiers)
         else:
@@ -81,12 +79,20 @@ def quorum_device_heuristics_cmd(lib, argv, modifiers):
 
 
 def quorum_config_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --corosync_conf - mocked corosync configuration file
+    """
+    modifiers.ensure_only_supported("--corosync_conf")
     if argv:
         raise CmdLineInputError()
     config = lib.quorum.get_config()
     print("\n".join(quorum_config_to_str(config)))
 
 def quorum_config_to_str(config):
+    """
+    Commandline options: no options
+    """
     lines = []
 
     lines.append("Options:")
@@ -128,24 +134,43 @@ def quorum_config_to_str(config):
     return lines
 
 def quorum_expected_votes_cmd(lib, argv, modifiers):
+    """
+    Options: no options
+    """
+    modifiers.ensure_only_supported()
     if len(argv) != 1:
         raise CmdLineInputError()
     lib.quorum.set_expected_votes_live(argv[0])
 
 def quorum_status_cmd(lib, argv, modifiers):
+    """
+    Options: no options
+    """
+    modifiers.ensure_only_supported()
     if argv:
         raise CmdLineInputError()
     print(lib.quorum.status())
 
 def quorum_update_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --skip-offline - skip offline nodes
+      * --force - force changes
+      * --corosync_conf - mocked corosync configuration file
+      * --request-timeout - HTTP timeout, has effect only if --corosync_conf is
+        not specified
+    """
+    modifiers.ensure_only_supported(
+        "--skip-offline", "--force", "--corosync_conf", "--request-timeout"
+    )
     options = parse_args.prepare_options(argv)
     if not options:
         raise CmdLineInputError()
 
     lib.quorum.set_options(
         options,
-        skip_offline_nodes=modifiers["skip_offline_nodes"],
-        force=modifiers["force"]
+        skip_offline_nodes=modifiers.get("--skip-offline"),
+        force=modifiers.get("--force"),
     )
 
 def _parse_quorum_device_groups(arg_list):
@@ -167,6 +192,17 @@ def _parse_quorum_device_groups(arg_list):
     return groups
 
 def quorum_device_add_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --force - allow unknown model or options
+      * --skip-offline - skip offline nodes
+      * --request-timeout - HTTP timeout, has effect only if --corosync_conf is
+        not specified
+      * --corosync_conf - mocked corosync configuration file
+    """
+    modifiers.ensure_only_supported(
+        "--force", "--skip-offline", "--request-timeout", "--corosync_conf"
+    )
     groups = _parse_quorum_device_groups(argv)
     model_and_model_options = groups.get("model", [])
     # we expect "model" keyword once, followed by the actual model value
@@ -188,25 +224,51 @@ def quorum_device_add_cmd(lib, argv, modifiers):
         model_options,
         generic_options,
         heuristics_options,
-        force_model=modifiers["force"],
-        force_options=modifiers["force"],
-        skip_offline_nodes=modifiers["skip_offline_nodes"]
+        force_model=modifiers.get("--force"),
+        force_options=modifiers.get("--force"),
+        skip_offline_nodes=modifiers.get("--skip-offline"),
     )
 
 def quorum_device_remove_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --skip-offline - skip offline nodes
+      * --corosync_conf - mocked corosync configuration file
+      * --request-timeout - HTTP timeout, has effect only if --corosync_conf is
+        not specified
+    """
+    modifiers.ensure_only_supported(
+        "--skip-offline", "--request-timeout", "--corosync_conf"
+    )
     if argv:
         raise CmdLineInputError()
 
     lib.quorum.remove_device(
-        skip_offline_nodes=modifiers["skip_offline_nodes"]
+        skip_offline_nodes=modifiers.get("--skip-offline")
     )
 
 def quorum_device_status_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --full - more detailed output
+    """
+    modifiers.ensure_only_supported("--full")
     if argv:
         raise CmdLineInputError()
-    print(lib.quorum.status_device(modifiers["full"]))
+    print(lib.quorum.status_device(modifiers.get("--full")))
 
 def quorum_device_update_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --force - allow unknown options
+      * --skip-offline - skip offline nodes
+      * --corosync_conf - mocked corosync configuration file
+      * --request-timeout - HTTP timeout, has effect only if --corosync_conf is
+        not specified
+    """
+    modifiers.ensure_only_supported(
+        "--force", "--skip-offline", "--request-timeout", "--corosync_conf"
+    )
     groups = _parse_quorum_device_groups(argv)
     if not groups:
         raise CmdLineInputError()
@@ -223,20 +285,37 @@ def quorum_device_update_cmd(lib, argv, modifiers):
         model_options,
         generic_options,
         heuristics_options,
-        force_options=modifiers["force"],
-        skip_offline_nodes=modifiers["skip_offline_nodes"]
+        force_options=modifiers.get("--force"),
+        skip_offline_nodes=modifiers.get("--skip-offline"),
     )
 
 def quorum_device_heuristics_remove_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --skip-offline - skip offline nodes
+      * --corosync_conf - mocked corosync configuration file
+      * --request-timeout - HTTP timeout, has effect only if --corosync_conf is
+        not specified
+    """
+    modifiers.ensure_only_supported(
+        "--skip-offline", "--corosync_conf", "--request-timeout"
+    )
     if argv:
         raise CmdLineInputError()
-    lib.quorum.remove_device_heuristics()
+    lib.quorum.remove_device_heuristics(
+        skip_offline_nodes=modifiers.get("--skip-offline"),
+    )
 
 # TODO switch to new architecture, move to lib
-def quorum_unblock_cmd(argv):
+def quorum_unblock_cmd(lib, argv, modifiers):
+    """
+    Options:
+      * --force - no error when removing non existing property and no warning
+        about this action
+    """
+    modifiers.ensure_only_supported("--force")
     if len(argv) > 0:
-        usage.quorum(["unblock"])
-        sys.exit(1)
+        raise CmdLineInputError()
 
     output, retval = utils.run(
         ["corosync-cmapctl", "-g", "runtime.votequorum.wait_for_all_status"]
@@ -253,7 +332,7 @@ def quorum_unblock_cmd(argv):
     )
     if not unjoined_nodes:
         utils.err("no unjoined nodes found")
-    if "--force" not in utils.pcs_options:
+    if modifiers.get("--force"):
         answer = utils.get_terminal_input(
             (
                 "WARNING: If node(s) {nodes} are not powered off or they do"
@@ -266,7 +345,10 @@ def quorum_unblock_cmd(argv):
             print("Canceled")
             return
     for node in unjoined_nodes:
-        stonith.stonith_confirm([node], skip_question=True)
+        # pass --force so no warning will be displayed
+        stonith.stonith_confirm(
+            lib, [node], parse_args.InputModifiers({"--force": ""})
+        )
 
     output, retval = utils.run(
         ["corosync-cmapctl", "-s", "quorum.cancel_wait_for_all", "u8", "1"]

pcs/acl.py
@@ -22,9 +22,9 @@ def acl_cmd(lib, argv, modifiers):
         elif sub_cmd == "show":
             show_acl_config(lib, argv_next, modifiers)
         elif sub_cmd == "enable":
-            acl_enable(argv_next)
+            acl_enable(lib, argv_next, modifiers)
         elif sub_cmd == "disable":
-            acl_disable(argv_next)
+            acl_disable(lib, argv_next, modifiers)
         elif sub_cmd == "role":
             acl_role(lib, argv_next, modifiers)
         elif sub_cmd in ["target", "user"]:
@@ -50,9 +50,14 @@ def _print_list_of_objects(obj_list, transformation_fn):
 
 
 def show_acl_config(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
     # TODO move to lib once lib supports cluster properties
     # enabled/disabled should be part of the structure returned
     # by lib.acl.get_config
+    modifiers.ensure_only_supported("-f")
     properties = utils.get_set_properties(defaults=prop.get_default_properties())
     acl_enabled = properties.get("enable-acl", "").lower()
     if is_true(acl_enabled):
@@ -67,13 +72,23 @@ def show_acl_config(lib, argv, modifiers):
     _print_list_of_objects(data.get("role_list", []), role_to_str)
 
 
-def acl_enable(argv):
+def acl_enable(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
     # TODO move to lib once lib supports cluster properties
-    prop.set_property(["enable-acl=true"])
+    modifiers.ensure_only_supported("-f")
+    prop.set_property(lib, ["enable-acl=true"], modifiers.get_subset("-f"))
 
-def acl_disable(argv):
+def acl_disable(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
     # TODO move to lib once lib supports cluster properties
-    prop.set_property(["enable-acl=false"])
+    modifiers.ensure_only_supported("-f")
+    prop.set_property(lib, ["enable-acl=false"], modifiers.get_subset("-f"))
 
 
 def acl_role(lib, argv, modifiers):
@@ -114,14 +129,24 @@ def acl_user(lib, argv, modifiers):
         utils.exit_on_cmdline_input_errror(e, "acl", "user {0}".format(sub_cmd))
 
 
-def user_create(lib, argv, dummy_modifiers):
+def user_create(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) < 1:
         raise CmdLineInputError()
     user_name, role_list = argv[0], argv[1:]
     lib.acl.create_target(user_name, role_list)
 
 
-def user_delete(lib, argv, dummy_modifiers):
+def user_delete(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) != 1:
         raise CmdLineInputError()
     lib.acl.remove_target(argv[0])
@@ -146,14 +171,24 @@ def acl_group(lib, argv, modifiers):
         )
 
 
-def group_create(lib, argv, dummy_modifiers):
+def group_create(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) < 1:
         raise CmdLineInputError()
     group_name, role_list = argv[0], argv[1:]
     lib.acl.create_group(group_name, role_list)
 
 
-def group_delete(lib, argv, dummy_modifiers):
+def group_delete(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) != 1:
         raise CmdLineInputError()
     lib.acl.remove_group(argv[0])
@@ -179,6 +214,9 @@ def acl_permission(lib, argv, modifiers):
 
 
 def argv_to_permission_info_list(argv):
+    """
+    Commandline options: no options
+    """
     if len(argv) % 3 != 0:
         raise CmdLineInputError()
 
@@ -203,6 +241,11 @@ def argv_to_permission_info_list(argv):
 
 
 def role_create(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) < 1:
         raise CmdLineInputError()
 
@@ -217,6 +260,11 @@ def role_create(lib, argv, modifiers):
 
 
 def role_delete(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) != 1:
         raise CmdLineInputError()
 
@@ -224,6 +272,9 @@ def role_delete(lib, argv, modifiers):
 
 
 def _role_assign_unassign(argv, keyword, not_specific_fn, user_fn, group_fn):
+    """
+    Commandline options: no options
+    """
     argv_len = len(argv)
     if argv_len < 2:
         raise CmdLineInputError()
@@ -250,7 +301,12 @@ def _role_assign_unassign(argv, keyword, not_specific_fn, user_fn, group_fn):
         raise CmdLineInputError()
 
 
-def role_assign(lib, argv, dummy_modifiers):
+def role_assign(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     _role_assign_unassign(
         argv,
         "to",
@@ -261,29 +317,45 @@ def role_assign(lib, argv, dummy_modifiers):
 
 
 def role_unassign(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+      * --autodelete - autodelete empty targets, groups
+    """
+    modifiers.ensure_only_supported("-f", "--autodelete")
     _role_assign_unassign(
         argv,
         "from",
         lambda role_id, ug_id: lib.acl.unassign_role_not_specific(
-            role_id, ug_id, modifiers.get("autodelete", False)
+            role_id, ug_id, modifiers.get("--autodelete")
         ),
         lambda role_id, ug_id: lib.acl.unassign_role_from_target(
-            role_id, ug_id, modifiers.get("autodelete", False)
+            role_id, ug_id, modifiers.get("--autodelete")
         ),
         lambda role_id, ug_id: lib.acl.unassign_role_from_group(
-            role_id, ug_id, modifiers.get("autodelete", False)
+            role_id, ug_id, modifiers.get("--autodelete")
         )
     )
 
 
-def permission_add(lib, argv, dummy_modifiers):
+def permission_add(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) < 4:
         raise CmdLineInputError()
     role_id, argv_next = argv[0], argv[1:]
     lib.acl.add_permission(role_id, argv_to_permission_info_list(argv_next))
 
 
-def run_permission_delete(lib, argv, dummy_modifiers):
+def run_permission_delete(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) != 1:
         raise CmdLineInputError()
     lib.acl.remove_permission(argv[0])

pcs/alert.py
@@ -67,8 +67,6 @@ def recipient_cmd(*args):
         )
 
 
-
-
 def ensure_only_allowed_options(parameter_dict, allowed_list):
     for arg, value in parameter_dict.items():
         if arg not in allowed_list:
@@ -78,6 +76,11 @@ def ensure_only_allowed_options(parameter_dict, allowed_list):
 
 
 def alert_add(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file (in lib wrapper)
+    """
+    modifiers.ensure_only_supported("-f")
     if not argv:
         raise CmdLineInputError()
 
@@ -95,6 +98,11 @@ def alert_add(lib, argv, modifiers):
 
 
 def alert_update(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file (in lib wrapper)
+    """
+    modifiers.ensure_only_supported("-f")
     if not argv:
         raise CmdLineInputError()
 
@@ -114,6 +122,11 @@ def alert_update(lib, argv, modifiers):
 
 
 def alert_remove(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file (in lib wrapper)
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) < 1:
         raise CmdLineInputError()
 
@@ -121,6 +134,12 @@ def alert_remove(lib, argv, modifiers):
 
 
 def recipient_add(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file (in lib wrapper)
+      * --force - allows not unique recipient values
+    """
+    modifiers.ensure_only_supported("-f", "--force")
     if len(argv) < 2:
         raise CmdLineInputError()
 
@@ -137,11 +156,17 @@ def recipient_add(lib, argv, modifiers):
         prepare_options(sections["meta"]),
         recipient_id=main_args.get("id", None),
         description=main_args.get("description", None),
-        allow_same_value=modifiers["force"]
+        allow_same_value=modifiers.get("--force")
     )
 
 
 def recipient_update(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file (in lib wrapper)
+      * --force - allows not unique recipient values
+    """
+    modifiers.ensure_only_supported("-f", "--force")
     if len(argv) < 1:
         raise CmdLineInputError()
 
@@ -157,11 +182,16 @@ def recipient_update(lib, argv, modifiers):
         prepare_options(sections["meta"]),
         recipient_value=main_args.get("value", None),
         description=main_args.get("description", None),
-        allow_same_value=modifiers["force"]
+        allow_same_value=modifiers.get("--force")
     )
 
 
 def recipient_remove(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file (in lib wrapper)
+    """
+    modifiers.ensure_only_supported("-f")
     if len(argv) < 1:
         raise CmdLineInputError()
 
@@ -216,6 +246,11 @@ def _recipient_to_str(recipient):
 
 
 def print_alert_config(lib, argv, modifiers):
+    """
+    Options:
+      * -f - CIB file (in lib wrapper)
+    """
+    modifiers.ensure_only_supported("-f")
     if argv:
         raise CmdLineInputError()
 
@@ -228,8 +263,14 @@ def print_alert_config(lib, argv, modifiers):
         print(" No alerts defined")
 
 
-def print_alerts_in_json(lib, argv, dummy_modifiers):
-    # This is used only by pcsd, will be removed in new architecture
+def print_alerts_in_json(lib, argv, modifiers):
+    """
+    This is used only by pcsd, will be removed in new architecture
+
+    Options:
+      * -f - CIB file (in lib wrapper)
+    """
+    modifiers.ensure_only_supported("-f")
     if argv:
         raise CmdLineInputError()
 

pcs/app.py
@@ -154,52 +154,16 @@ def main(argv=None):
         "stonith": stonith.stonith_cmd,
         "property": prop.property_cmd,
         "constraint": constraint.constraint_cmd,
-        "acl": lambda argv: acl.acl_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
-        "status": lambda argv: status.status_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
+        "acl": acl.acl_cmd,
+        "status": status.status_cmd,
         "config": config.config_cmd,
-        "pcsd": lambda argv: pcsd.pcsd_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
-        "node": lambda argv: node.node_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
-        "quorum": lambda argv: quorum.quorum_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
-        "qdevice": lambda argv: qdevice.qdevice_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
-        "alert": lambda args: alert.alert_cmd(
-            utils.get_library_wrapper(),
-            args,
-            utils.get_modifiers()
-        ),
-        "booth": lambda argv: booth.booth_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
-        "host": lambda argv: host.host_cmd(
-            utils.get_library_wrapper(),
-            argv,
-            utils.get_modifiers()
-        ),
+        "pcsd": pcsd.pcsd_cmd,
+        "node": node.node_cmd,
+        "quorum": quorum.quorum_cmd,
+        "qdevice": qdevice.qdevice_cmd,
+        "alert": alert.alert_cmd,
+        "booth": booth.booth_cmd,
+        "host": host.host_cmd,
     }
     if command not in cmd_map:
         usage.main()
@@ -207,7 +171,11 @@ def main(argv=None):
     # root can run everything directly, also help can be displayed,
     # working on a local file also do not need to run under root
     if (os.getuid() == 0) or (argv and argv[0] == "help") or usefile:
-        cmd_map[command](argv)
+        cmd_map[command](
+            utils.get_library_wrapper(),
+            argv,
+            utils.get_input_modifiers(),
+        )
         return
     # specific commands need to be run under root account, pass them to pcsd
     # don't forget to allow each command in pcsd.rb in "post /run_pcs do"
@@ -264,4 +232,8 @@ def main(argv=None):
                 sys.stderr.write(std_err)
             sys.exit(exitcode)
             return
-    cmd_map[command](argv)
+    cmd_map[command](
+        utils.get_library_wrapper(),
+        argv,
+        utils.get_input_modifiers(),
+    )

pcs/cli/booth/command.py
@@ -5,7 +5,16 @@ from pcs.cli.common.parse_args import group_by_keywords, prepare_options
 def config_setup(lib, arg_list, modifiers):
     """
     create booth config
+
+    Options:
+      * --force - overwrite existing
+      * --booth-conf - booth config file
+      * --booth-key - booth authkey file
+      * --name - name of a booth instance
     """
+    modifiers.ensure_only_supported(
+        "--force", "--booth-conf", "--booth-key", "--name",
+    )
     peers = group_by_keywords(
         arg_list,
         set(["sites", "arbitrators"]),
@@ -24,21 +33,41 @@ def config_setup(lib, arg_list, modifiers):
             "details": [],
         })
 
-    lib.booth.config_setup(booth_config, modifiers["force"])
+    lib.booth.config_setup(
+        booth_config, overwrite_existing=modifiers.get("--force")
+    )
 
 def config_destroy(lib, arg_list, modifiers):
     """
     destroy booth config
+
+    Options:
+      --force - ignore config load issues
+      --name - name of a booth instance
     """
+    modifiers.ensure_only_supported("--force", "--name")
     if arg_list:
         raise CmdLineInputError()
-    lib.booth.config_destroy(ignore_config_load_problems=modifiers["force"])
+    lib.booth.config_destroy(
+        ignore_config_load_problems=modifiers.get("--force")
+    )
 
 
 def config_show(lib, arg_list, modifiers):
     """
     print booth config
+
+    Options:
+      * --name - name of a booth instace
+      * --booth-conf - booth config file, effective only when no node is
+        specified
+      * --booth-key - booth auth key file, not required by the command (just
+        from middleware)
+      * --request-timeout - HTTP timeout for getting config from remote host
     """
+    modifiers.ensure_only_supported(
+        "--name", "--booth-conf", "--request-timeout", "--booth-key"
+    )
     if len(arg_list) > 1:
         raise CmdLineInputError()
     node = None if not arg_list else arg_list[0]
@@ -49,24 +78,46 @@ def config_show(lib, arg_list, modifiers):
 def config_ticket_add(lib, arg_list, modifiers):
     """
     add ticket to current configuration
+
+    Options:
+      * --force - ignore config load issues
+      * --booth-conf - booth config file
+      * --booth-key - booth auth key file, not required by the command (just
+        from middleware)
+      * --name - name of a booth instace
     """
+    modifiers.ensure_only_supported(
+        "--force", "--booth-conf", "--name", "--booth-key"
+    )
     if not arg_list:
         raise CmdLineInputError
     lib.booth.config_ticket_add(
         arg_list[0],
         prepare_options(arg_list[1:]),
-        allow_unknown_options=modifiers["force"]
+        allow_unknown_options=modifiers.get("--force")
     )
 
 def config_ticket_remove(lib, arg_list, modifiers):
     """
     add ticket to current configuration
+
+    Options:
+      * --booth-conf - booth config file
+      * --booth-key - booth auth key file, not required by the command (just
+        from middleware)
+      * --name - name of a booth instace
     """
+    modifiers.ensure_only_supported("--booth-conf", "--name", "--booth-key")
     if len(arg_list) != 1:
         raise CmdLineInputError
     lib.booth.config_ticket_remove(arg_list[0])
 
-def ticket_operation(lib_call, arg_list, modifiers):
+def _ticket_operation(lib_call, arg_list):
+    """
+    Commandline options:
+      * --name - name of a booth instance
+      * -f - CIB file
+    """
     site_ip = None
     if len(arg_list) == 2:
         site_ip = arg_list[1]
@@ -77,29 +128,56 @@ def ticket_operation(lib_call, arg_list, modifiers):
     lib_call(ticket, site_ip)
 
 def ticket_revoke(lib, arg_list, modifiers):
-    ticket_operation(lib.booth.ticket_revoke, arg_list, modifiers)
+    """
+    Options:
+      * --name - name of a booth instance
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f", "--name")
+    _ticket_operation(lib.booth.ticket_revoke, arg_list)
 
 def ticket_grant(lib, arg_list, modifiers):
-    ticket_operation(lib.booth.ticket_grant, arg_list, modifiers)
+    """
+    Options:
+      * --name - name of a booth instance
+      * -f - CIB file
+    """
+    modifiers.ensure_only_supported("-f", "--name")
+    _ticket_operation(lib.booth.ticket_grant, arg_list)
 
 def create_in_cluster(lib, arg_list, modifiers):
+    """
+    Options:
+      * --force - allows to create booth resource even if its agent is not
+        installed
+      * -f - CIB file
+      * --name - name of a booth instance
+    """
+    modifiers.ensure_only_supported("--force", "-f", "--name")
     if len(arg_list) != 2 or arg_list[0] != "ip":
         raise CmdLineInputError()
     lib.booth.create_in_cluster(
         ip=arg_list[1],
-        allow_absent_resource_agent=modifiers["force"]
+        allow_absent_resource_agent=modifiers.get("--force")
     )
 
 def get_remove_from_cluster(resource_remove):
     #TODO resource_remove is provisional hack until resources are not moved to
     #lib
     def remove_from_cluster(lib, arg_list, modifiers):
+        """
+        Options:
+          * --force - allow remove of multiple
+          * -f - CIB file
+          * --name - name of a booth instance
+        """
+        modifiers.ensure_only_supported("--force", "-f", "--name")
         if arg_list:
             raise CmdLineInputError()
 
         lib.booth.remove_from_cluster(
             resource_remove,
-            modifiers["force"],
+            allow_remove_multiple=modifiers.get("--force"),
         )
 
     return remove_from_cluster
@@ -108,50 +186,103 @@ def get_restart(resource_restart):
     #TODO resource_restart is provisional hack until resources are not moved to
     #lib
     def restart(lib, arg_list, modifiers):
+        """
+        Options:
+          * --force - allow multiple
+          * -f - CIB file
+          * --name - name of a booth instance
+        """
+        modifiers.ensure_only_supported("--force", "-f", "--name")
         if arg_list:
             raise CmdLineInputError()
 
-        lib.booth.restart(resource_restart, modifiers["force"])
+        lib.booth.restart(
+            resource_restart,
+            allow_multiple=modifiers.get("--force"),
+        )
 
     return restart
 
 def sync(lib, arg_list, modifiers):
+    """
+    Options:
+      * --skip-offline - skip offline nodes
+      * --name - name of a booth instance
+      * --booth-conf - booth config file
+      * --booth-key - booth authkey file
+      * --request-timeout - HTTP timeout for file ditribution
+    """
+    modifiers.ensure_only_supported(
+        "--skip-offline", "--name", "--booth-conf", "--booth-key",
+        "--request-timeout",
+    )
     if arg_list:
         raise CmdLineInputError()
-    lib.booth.config_sync(skip_offline_nodes=modifiers["skip_offline_nodes"])
+    lib.booth.config_sync(skip_offline_nodes=modifiers.get("--skip-offline"))
 
 
 def enable(lib, arg_list, modifiers):
+    """
+    Options:
+      * --name - name of a booth instance
+    """
+    modifiers.ensure_only_supported("--name")
     if arg_list:
         raise CmdLineInputError()
     lib.booth.enable()
 
 
 def disable(lib, arg_list, modifiers):
+    """
+    Options:
+      * --name - name of a booth instance
+    """
+    modifiers.ensure_only_supported("--name")
     if arg_list:
         raise CmdLineInputError()
     lib.booth.disable()
 
 
 def start(lib, arg_list, modifiers):
+    """
+    Options:
+      * --name - name of a booth instance
+    """
+    modifiers.ensure_only_supported("--name")
     if arg_list:
         raise CmdLineInputError()
     lib.booth.start()
 
 
 def stop(lib, arg_list, modifiers):
+    """
+    Options:
+      * --name - name of a booth instance
+    """
+    modifiers.ensure_only_supported("--name")
     if arg_list:
         raise CmdLineInputError()
     lib.booth.stop()
 
 
 def pull(lib, arg_list, modifiers):
+    """
+    Options:
+      * --name - name of a booth instance
+      * --request-timeout - HTTP timeout for file ditribution
+    """
+    modifiers.ensure_only_supported("--name", "--request-timeout")
     if len(arg_list) != 1:
         raise CmdLineInputError()
     lib.booth.pull(arg_list[0])
 
 
 def status(lib, arg_list, modifiers):
+    """
+    Options:
+      * --name - name of booth instance
+    """
+    modifiers.ensure_only_supported("--name")
     if arg_list:
         raise CmdLineInputError()
     booth_status = lib.booth.status()

commit fff9c1f0ab6530c6c916e1367bab9c42f77d81d7

fix --force in 'pcs quorum unblock'
broken in 61d7feb018db5dd21165b05c7f64ab85720cf0f6

pcs/quorum.py
@@ -264,7 +264,7 @@ def quorum_unblock_cmd(lib, argv, modifiers):
     unjoined_nodes = set(all_nodes) - set(utils.getCorosyncActiveNodes())
     if not unjoined_nodes:
         utils.err("no unjoined nodes found")
-    if modifiers.get("--force"):
+    if not modifiers.get("--force"):
         answer = utils.get_terminal_input(
             (
                 "WARNING: If node(s) {nodes} are not powered off or they do"