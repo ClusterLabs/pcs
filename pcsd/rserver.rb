4f9110e4b260adbcf0f6b5026fb5086f82210863
Ivan Devat <idevat@redhat.com>
2020-02-11 15:31:20 +0100

    cleanup ruby server code

--- pcsd/rserver.rb
+++ pcsd/rserver.rb
@@ -7,9 +7,14 @@ require 'thin'
 
 require 'settings.rb'
 
-def tm
-  t1 = Time.now
-  " #{t1.min}:#{t1.sec}.#{t1.usec}"
+def pack_response(response)
+  return [200, {}, [response.to_json.to_str]]
+end
+
+def unpack_request(transport_env)
+  return JSON.parse(Base64.strict_decode64(
+    transport_env["rack.request.form_hash"]["TORNADO_REQUEST"]
+  ))
 end
 
 class TornadoCommunicationMiddleware
@@ -17,65 +22,51 @@ class TornadoCommunicationMiddleware
     @app = app
   end
 
-  def call(env)
-    tornado_logs = []
-    Thread.current[:pcsd_logger_container] = tornado_logs
-    id = (0...4).map { (65 + rand(26)).chr }.join
-    # raw_data = request.env["rack.input"].read
-    # puts raw_data
-    tornado_request = Base64.strict_decode64(env["rack.request.form_hash"]["TORNADO_REQUEST"]);
-    #TODO begin - rescue for JSON.parse
-    #TODO check if requests includes key "type" - see sinatra_cmdline_wrapper.rb
-    #TODO sinatra "use" settings - see sinatra_cmdline_wrapper.rb
-    # puts tornado_request
-    request = JSON.parse(tornado_request)
-    if ["sinatra_gui", "sinatra_remote"].include?(request["type"])
-      if request["type"] == "sinatra_gui"
-        Thread.current[:tornado_username] = request["session"]["username"]
-        Thread.current[:tornado_groups] = request["session"]["groups"]
-        Thread.current[:tornado_is_authenticated] = request["session"]["is_authenticated"]
+  def call(transport_env)
+    Thread.current[:pcsd_logger_container] = []
+    begin
+      request = unpack_request(transport_env)
+
+      if ["sinatra_gui", "sinatra_remote"].include?(request["type"])
+        if request["type"] == "sinatra_gui"
+          session = request["session"]
+          Thread.current[:tornado_username] = session["username"]
+          Thread.current[:tornado_groups] = session["groups"]
+          Thread.current[:tornado_is_authenticated] = session["is_authenticated"]
+        end
+
+        # Keys rack.input and rack.errors are required. We make sure they are
+        # there.
+        request_env = request["env"]
+        request_env["rack.input"] = StringIO.new(request_env["rack.input"])
+        request_env["rack.errors"] = StringIO.new()
+
+        status, headers, body = @app.call(request_env)
+
+        rack_errors = request_env['rack.errors'].string()
+        if not rack_errors.empty?()
+          $logger.error(rack_errors)
+        end
+
+        return pack_response({
+          :status => status,
+          :headers => headers,
+          :body => Base64.encode64(body.join("")),
+          :logs => Thread.current[:pcsd_logger_container],
+        })
       end
 
-      # set :logging, true
-      # set :run, false
-      # Do not turn exceptions into fancy 100kB HTML pages and print them on stdout.
-      # Instead, rack.errors is logged and therefore returned in result[:log].
-      # set :show_exceptions, false
-
-      env = request["env"]
-      env["rack.input"] = StringIO.new(env["rack.input"])
-      env["rack.errors"] = StringIO.new()
-
-      puts id + tm() +" "+request["env"]["PATH_INFO"]
-      status, headers, body = @app.call(env)
-      rack_errors = env['rack.errors'].string()
-      if not rack_errors.empty?()
-        $logger.error(rack_errors)
+      if request["type"] == "sync_configs"
+        return pack_response({
+          :next => Time.now.to_i + run_cfgsync(),
+          :logs => Thread.current[:pcsd_logger_container],
+        })
       end
 
-      puts id + tm() +" "+status.to_s
-      # puts id+" "+body.join("")
-      # tornado_logs.each{|log|
-      #   puts id+" "+log[:level]+" "+log[:message].to_s
-      # }
-      # puts "=================================================================="
-      result = {
-        :status => status,
-        :headers => headers,
-        :body => Base64.encode64(body.join("")),
-      }
-
-    elsif request["type"] == "sync_configs"
-      result = {
-        :next => Time.now.to_i + run_cfgsync()
-      }
-    else
-      result = {:error => "Unknown type: '#{request["type"]}'"}
+      raise "Unexpected value for key 'type': '#{request['type']}'"
+    rescue => e
+      return pack_response({:error => "Processing request error: '#{e}'"})
     end
-
-    result[:logs] = tornado_logs
-
-    [200, {}, [result.to_json.to_str]]
   end
 end
 
