2aecfa7f5552f6a080d778c3a4f18ed91767cc05
Miroslav Lisik <mlisik@redhat.com>
2020-05-14 15:01:17 +0200

    add test coverage

--- pcs_test/tools/fixture.py
+++ pcs_test/tools/fixture.py
@@ -2,6 +2,7 @@ import json
 
 from pcs.common.reports import ReportItemSeverity as severities
 from pcs.common.reports import codes as report_codes
+from pcs.lib.cib.resource.common import ALL_RESOURCE_XML_TAGS
 
 
 def state_node(
@@ -194,17 +195,19 @@ def report_not_found(
             "context_type": context_type,
             "context_id": context_id,
             "id": res_id,
-            "expected_types": [
-                "bundle",
-                "clone",
-                "group",
-                "master",
-                "primitive",
-            ]
-            if expected_types is None
-            else expected_types,
+            "expected_types": (
+                ALL_RESOURCE_XML_TAGS
+                if expected_types is None
+                else expected_types
+            ),
         },
         None,
+    )
+
+
+def report_not_resource_or_tag(element_id):
+    return report_not_found(
+        element_id, expected_types=sorted(ALL_RESOURCE_XML_TAGS + ["tag"])
     )
 
 
