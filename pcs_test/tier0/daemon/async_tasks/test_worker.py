329697c95b1bebd48a52c7c6cfdc1fc92296860b
Michal Pospisil <mpospisi@redhat.com>
2020-11-17 17:14:34 +0100

    WIP: Worker tests

---
+++ pcs_test/tier0/daemon/async_tasks/test_worker.py
@@ -0,0 +1,110 @@
+from multiprocessing import Queue
+from unittest import mock, TestCase
+
+import pcs.common.async_tasks.types as types
+import pcs.daemon.async_tasks.messaging as messaging
+import pcs.daemon.async_tasks.worker as worker
+
+from pcs.common.async_tasks.dto import CommandDto
+from pcs.common.reports import ReportItemDto
+from .test_command_mapping import test_command_map
+
+
+TASK_IDENT1 = "10000000000000000123456789abcdef"
+TASK_IDENT2 = "20000000000000000123456789abcdef"
+TASK_IDENT3 = "30000000000000000123456789abcdef"
+TASK_IDENT4 = "40000000000000000123456789abcdef"
+WORKER_PID = 2222
+
+worker.worker_com = Queue()  # preventing parallel tests
+
+
+@mock.patch("pcs.daemon.async_tasks.worker.command_map", test_command_map)
+@mock.patch("os.getpid")
+class TestExecutor(TestCase):
+    def _assert_executor_not_raises_exc(self, worker_command):
+        try:
+            worker.task_executor(worker_command)
+        except Exception:
+            self.fail("Task executor cannot raise exceptions.")
+
+    def _get_payload_from_worker_com(self, worker_com, task_ident):
+        while True:
+            message = worker_com.get()
+            self.assertTrue(isinstance(message, messaging.Message))
+            if message.task_ident == task_ident:
+                break
+            worker_com.put(message)
+        return message.payload
+
+    def _assert_task_executed(self, worker_com, task_ident):
+        payload = self._get_payload_from_worker_com(worker_com, task_ident)
+        self.assertTrue(isinstance(payload, messaging.TaskExecuted))
+        self.assertEqual(WORKER_PID, payload.worker_pid)
+
+    def test_successful_run(self, mock_getpid):
+        mock_getpid.return_value = WORKER_PID
+        self._assert_executor_not_raises_exc(
+            worker.WorkerCommand(TASK_IDENT1, CommandDto("success", {}))
+        )
+        # 1. TaskExecuted
+        self._assert_task_executed(worker.worker_com, TASK_IDENT1)
+        # 2. TaskFinished
+        payload = self._get_payload_from_worker_com(
+            worker.worker_com, TASK_IDENT1
+        )
+        self.assertTrue(isinstance(payload, messaging.TaskFinished))
+        self.assertEqual(types.TaskFinishType.SUCCESS, payload.task_finish_type)
+        self.assertEqual("I'm done.", payload.result)
+
+    def test_unsuccessful_run(self, mock_getpid):
+        mock_getpid.return_value = WORKER_PID
+        self._assert_executor_not_raises_exc(
+            worker.WorkerCommand(TASK_IDENT2, CommandDto("lib_exc", {}))
+        )
+        # 1. TaskExecuted
+        self._assert_task_executed(worker.worker_com, TASK_IDENT2)
+        # 2. TaskFinished
+        payload = self._get_payload_from_worker_com(
+            worker.worker_com, TASK_IDENT2
+        )
+        self.assertTrue(isinstance(payload, messaging.TaskFinished))
+        self.assertEqual(types.TaskFinishType.FAIL, payload.task_finish_type)
+        self.assertEqual(None, payload.result)
+
+    def test_unsuccessful_run_additional_reports(self, mock_getpid):
+        mock_getpid.return_value = WORKER_PID
+        self._assert_executor_not_raises_exc(
+            worker.WorkerCommand(TASK_IDENT3, CommandDto("lib_exc_reports", {}))
+        )
+        # 1. TaskExecuted
+        self._assert_task_executed(worker.worker_com, TASK_IDENT3)
+        # 2. Report from the LibraryError exception
+        payload = self._get_payload_from_worker_com(
+            worker.worker_com, TASK_IDENT3
+        )
+        self.assertTrue(isinstance(payload, ReportItemDto))
+        # 3. TaskFinished
+        payload = self._get_payload_from_worker_com(
+            worker.worker_com, TASK_IDENT3
+        )
+        self.assertTrue(isinstance(payload, messaging.TaskFinished))
+        self.assertEqual(types.TaskFinishType.FAIL, payload.task_finish_type)
+        self.assertEqual(None, payload.result)
+
+    def test_unhandled_exception(self, mock_getpid):
+        mock_getpid.return_value = WORKER_PID
+        self._assert_executor_not_raises_exc(
+            worker.WorkerCommand(TASK_IDENT4, CommandDto("unhandled_exc", {}))
+        )
+        # 1. TaskExecuted
+        self._assert_task_executed(worker.worker_com, TASK_IDENT4)
+        # 2. TaskFinished
+        payload = self._get_payload_from_worker_com(
+            worker.worker_com, TASK_IDENT4
+        )
+        self.assertTrue(isinstance(payload, messaging.TaskFinished))
+        self.assertEqual(
+            types.TaskFinishType.UNHANDLED_EXCEPTION, payload.task_finish_type
+        )
+        self.assertEqual(None, payload.result)
