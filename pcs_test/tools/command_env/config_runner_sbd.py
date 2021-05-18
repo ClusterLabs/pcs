84900b64df8ed00736927eef0fedf2f6c55e7d84
Ondrej Mular <omular@redhat.com>
2021-05-18 10:12:10 +0200

    tests: sbd: convert test to use testing framework

--- pcs_test/tools/command_env/config_runner_sbd.py
+++ pcs_test/tools/command_env/config_runner_sbd.py
@@ -1,6 +1,7 @@
 from pcs_test.tools.command_env.mock_runner import Call as RunnerCall
 
 from pcs import settings
+from pcs.lib.sbd import DEVICE_INITIALIZATION_OPTIONS_MAPPING
 
 
 class SbdShortcuts:
@@ -26,4 +27,87 @@ class SbdShortcuts:
             ),
             before=before,
             instead=instead,
+        )
+
+    def initialize_devices(
+        self,
+        devices,
+        options,
+        stdout="",
+        stderr="",
+        return_code=0,
+        name="runner.sbd.initialize_devices",
+    ):
+        cmd = [settings.sbd_binary]
+        for device in devices:
+            cmd += ["-d", device]
+
+        for opt, val in sorted(options.items()):
+            cmd += [DEVICE_INITIALIZATION_OPTIONS_MAPPING[opt], str(val)]
+
+        cmd.append("create")
+        self.__calls.place(
+            name,
+            RunnerCall(
+                cmd,
+                stdout=stdout,
+                stderr=stderr,
+                returncode=return_code,
+            ),
+        )
+
+    def get_device_info(
+        self,
+        device,
+        stdout="",
+        stderr="",
+        return_code=0,
+        name="runner.sbd.device.list",
+    ):
+        self.__calls.place(
+            name,
+            RunnerCall(
+                [settings.sbd_binary, "-d", device, "list"],
+                stdout=stdout,
+                stderr=stderr,
+                returncode=return_code,
+            ),
+        )
+
+    def get_device_dump(
+        self,
+        device,
+        stdout="",
+        stderr="",
+        return_code=0,
+        name="runner.sbd.device.dump",
+    ):
+        self.__calls.place(
+            name,
+            RunnerCall(
+                [settings.sbd_binary, "-d", device, "dump"],
+                stdout=stdout,
+                stderr=stderr,
+                returncode=return_code,
+            ),
+        )
+
+    def set_device_message(
+        self,
+        device,
+        node,
+        message,
+        stdout="",
+        stderr="",
+        return_code=0,
+        name="runner.sbd.device.set_message",
+    ):
+        self.__calls.place(
+            name,
+            RunnerCall(
+                [settings.sbd_binary, "-d", device, "message", node, message],
+                stdout=stdout,
+                stderr=stderr,
+                returncode=return_code,
+            ),
         )
