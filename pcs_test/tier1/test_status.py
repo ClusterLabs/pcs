e2a230143144393ec004f25e832964389f0eeec8
Michal Pospisil <mpospisi@redhat.com>
2021-10-13 13:45:08 +0200

    do not show stonith warning when stonith device is in a group
    If a stonith device was in a group, `pcs cluster status` would show
    a warning that no stonith device was detected and stonith-enabled
    was not false.

--- pcs_test/tier1/test_status.py
+++ pcs_test/tier1/test_status.py
@@ -4,6 +4,7 @@ from textwrap import dedent
 from unittest import TestCase
 
 from pcs_test.tools.assertions import AssertPcsMixin
+from pcs_test.tools.bin_mock import get_mock_settings
 from pcs_test.tools.misc import (
     get_test_resource as rc,
     get_tmp_file,
@@ -187,6 +188,33 @@ class StonithWarningTest(TestCase, AssertPcsMixin):
 
                     WARNINGS:
                     No stonith devices and stonith-enabled is not false
+
+                    Stack: unknown
+                    Current DC: NONE
+                """
+                ),
+            )
+
+    def test_no_stonith_warning_when_stonith_in_group(self):
+        self.assert_pcs_success("stonith create S fence_xvm --group G".split())
+        self.pcs_runner.corosync_conf_opt = self.corosync_conf
+        self.pcs_runner.mock_settings = get_mock_settings("crm_resource_binary")
+        if PCMK_2_0_3_PLUS:
+            self.assert_pcs_success(
+                ["status"],
+                stdout_start=dedent(
+                    """\
+                    Cluster name: test99
+                    Cluster Summary:
+                """
+                ),
+            )
+        else:
+            self.assert_pcs_success(
+                ["status"],
+                stdout_start=dedent(
+                    """\
+                    Cluster name: test99
 
                     Stack: unknown
                     Current DC: NONE
