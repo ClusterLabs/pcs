df87b1c92f0472a786ec9b3d6c18577af7c764ea
Ondrej Mular <omular@redhat.com>
2022-04-14 10:35:16 +0200

    add worker pid to log messages

--- pcs/daemon/async_tasks/logging.py
+++ pcs/daemon/async_tasks/logging.py
@@ -1,5 +1,36 @@
 import logging
 import multiprocessing as mp
+import os
+
+
+class Logger(logging.Logger):
+    # pylint: disable=too-many-arguments
+    def makeRecord(  # type: ignore
+        self,
+        name,
+        level,
+        fn,
+        lno,
+        msg,
+        args,
+        exc_info,
+        func=None,
+        extra=None,
+        sinfo=None,
+    ) -> logging.LogRecord:
+        pid = os.getpid()
+        return super().makeRecord(
+            name,
+            level,
+            fn,
+            lno,
+            f"Worker#{pid}: {msg}",
+            args,
+            exc_info,
+            func=func,
+            extra=extra,
+            sinfo=sinfo,
+        )
 
 
 def setup_worker_logger(queue: mp.Queue) -> logging.Logger:
@@ -7,10 +38,11 @@ def setup_worker_logger(queue: mp.Queue) -> logging.Logger:
     Creates and configures worker's logger
     :return: Logger instance
     """
+    logging.setLoggerClass(Logger)
     logger = logging.getLogger("pcs_worker")
     logger.setLevel(logging.DEBUG)
 
-    queue_handler = logging.handlers.QueueHandler(queue)
+    queue_handler = logging.handlers.QueueHandler(queue)  # type: ignore
     queue_handler.setLevel(logging.DEBUG)
     logger.addHandler(queue_handler)
 
