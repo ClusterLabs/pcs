89c6d8ce90e032ed16c8db3d8148f6b41d617fea
mengfansheng <mengfansheng@chengjieos.com>
2022-04-25 09:54:07 +0800

    fix bug 1899519

--- pcs/lib/cib/nvpair.py
+++ pcs/lib/cib/nvpair.py
@@ -32,9 +32,27 @@ def _append_new_nvpair(nvset_element, name, value, id_provider):
         "nvpair",
         id=create_subelement_id(nvset_element, name, id_provider),
         name=name,
-        value=value,
+        value=clean_xml_string(value)
     )
 
+def is_valid_xml_char_ordinal(i):
+    """
+    Defines whether char is valid to use in xml document
+    XML standard defines a valid char as::
+    Char ::= #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
+    """
+    return ( # conditions ordered by presumed frequency
+        0x20 <= i <= 0xD7FF
+        or i in (0x9, 0xA, 0xD)
+        or 0xE000 <= i <= 0xFFFD
+        or 0x10000 <= i <= 0x10FFFF
+        )
+
+def clean_xml_string(s):
+    """
+    Cleans string from invalid xml chars
+    """
+    return ''.join(c for c in s if is_valid_xml_char_ordinal(ord(c)))
 
 def set_nvpair_in_nvset(nvset_element, name, value, id_provider):
     """
