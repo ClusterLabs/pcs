86a16b400a9308eb0bd4168b0e67e200e1d7cf04
Ondrej Mular <omular@redhat.com>
2022-05-08 14:46:07 +0200

    fix CLI report processor

--- pcs/cli/reports/processor.py
+++ pcs/cli/reports/processor.py
@@ -25,31 +25,36 @@ from .output import (
 class ReportProcessorToConsole(ReportProcessor):
     def __init__(self, debug: bool = False) -> None:
         super().__init__()
-        self._ignore_severities: Set[SeverityLevel] = set()
         self.debug = debug
+        self._ignore_severities = self._get_ignored_severities([])
 
     def _do_report(self, report_item: ReportItem) -> None:
         report_item_dto = report_item.to_dto()
-        if report_item_dto.severity.level in self._ignore_severities:
-            return
-        print_report(report_item_dto)
+        if report_item_dto.severity.level not in self._ignore_severities:
+            print_report(report_item_dto)
+
+    def _get_ignored_severities(
+        self, supressed_severity_list: Iterable[SeverityLevel]
+    ) -> Set[SeverityLevel]:
+        ignore_severities = set(supressed_severity_list)
+        if self.debug:
+            ignore_severities -= {ReportItemSeverity.DEBUG}
+        else:
+            ignore_severities |= {ReportItemSeverity.DEBUG}
+        return ignore_severities
 
     def suppress_reports_of_severity(
         self, severity_list: Iterable[SeverityLevel]
     ) -> None:
-        self._ignore_severities = set(severity_list)
-        if self.debug:
-            self._ignore_severities -= {ReportItemSeverity.DEBUG}
-        else:
-            self._ignore_severities |= {ReportItemSeverity.DEBUG}
+        self._ignore_severities = self._get_ignored_severities(severity_list)
 
 
 def print_report(report_item_dto: ReportItemDto) -> None:
     msg = report_item_msg_from_dto(report_item_dto.message).message
-    if report_item_dto.context:
-        msg = f"{report_item_dto.context.node}: {msg}"
     if not msg:
         return
+    if report_item_dto.context:
+        msg = f"{report_item_dto.context.node}: {msg}"
 
     severity = report_item_dto.severity.level
     if severity == ReportItemSeverity.ERROR:
