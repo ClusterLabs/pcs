c9061654fbde47421bc3dfdc37e4381a52543d83
Ondrej Mular <omular@redhat.com>
2022-06-16 15:30:45 +0200

    ignore debug reports in worker

--- pcs/daemon/async_tasks/worker/report_processor.py
+++ pcs/daemon/async_tasks/worker/report_processor.py
@@ -9,12 +9,23 @@ class WorkerReportProcessor(pcs_reports.ReportProcessor):
     Report processor for tasks running inside of the worker pool
     """
 
-    def __init__(self, worker_com: WorkerCommunicator, task_ident: str) -> None:
+    def __init__(
+        self,
+        worker_com: WorkerCommunicator,
+        task_ident: str,
+        enable_debug: bool = False,
+    ) -> None:
         super().__init__()
         self._worker_communicator = worker_com
         self._task_ident: str = task_ident
+        self._debug_enabled = enable_debug
 
     def _do_report(self, report_item: pcs_reports.item.ReportItem) -> None:
-        self._worker_communicator.put(
-            Message(self._task_ident, report_item.to_dto())
-        )
+        if (
+            self._debug_enabled
+            or report_item.severity.level
+            != pcs_reports.ReportItemSeverity.DEBUG
+        ):
+            self._worker_communicator.put(
+                Message(self._task_ident, report_item.to_dto())
+            )
