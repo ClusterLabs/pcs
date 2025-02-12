ClusterLabs/pcs

commit ccffba735128ea4be62aa33e7319114d8b26a8b0

move resource restart to pcs.lib

pcs/lib/commands/resource.py
@@ -66,6 +66,7 @@ from pcs.lib.pacemaker.live import (
     push_cib_diff_xml,
     resource_ban,
     resource_move,
+    resource_restart,
     resource_unmove_unban,
     simulate_cib,
 )
@@ -101,7 +102,7 @@ from pcs.lib.xml_tools import (
 
 @contextmanager
 def resource_environment(
-    env,
+    env: LibraryEnvironment,
     wait: WaitType = False,
     wait_for_resource_ids=None,
     resource_state_reporter=info_resource_state,
@@ -2541,4 +2542,85 @@ def get_configured_resources(env: LibraryEnvironment) -> CibResourcesDto:
             )
         ],
         bundles=bundles,
+    )
+
+
+def restart(
+    env: LibraryEnvironment,
+    resource_id: str,
+    node: Optional[str] = None,
+    timeout: Optional[str] = None,
+) -> None:
+    """
+    Restart a resource
+
+    resource_id -- id of the resource to be restarted
+    node -- name of the node to limit the restart to
+    timeout -- abort if the command doesn't finish in this time (integer + unit)
+    """
+    cib = env.get_cib()
+    try:
+        resource_el = get_element_by_id(cib, resource_id)
+    except ElementNotFound as e:
+        env.report_processor.report(
+            ReportItem.error(
+                reports.messages.IdNotFound(
+                    resource_id, expected_types=["resource"]
+                )
+            )
+        )
+        raise LibraryError() from e
+    if not resource.common.is_resource(resource_el):
+        env.report_processor.report(
+            ReportItem.error(
+                reports.messages.IdBelongsToUnexpectedType(
+                    resource_id,
+                    expected_types=["resource"],
+                    current_type=resource_el.tag,
+                )
+            )
+        )
+        raise LibraryError()
+
+    parent_resource_el = resource.clone.get_parent_any_clone(resource_el)
+    if parent_resource_el is None:
+        parent_resource_el = resource.bundle.get_parent_bundle(resource_el)
+    if parent_resource_el is not None:
+        env.report_processor.report(
+            reports.ReportItem.warning(
+                reports.messages.ResourceRestartUsingParentRersource(
+                    str(resource_el.attrib["id"]),
+                    str(parent_resource_el.attrib["id"]),
+                )
+            )
+        )
+        resource_el = parent_resource_el
+
+    if node and not (
+        resource.clone.is_any_clone(resource_el)
+        or resource.bundle.is_bundle(resource_el)
+    ):
+        env.report_processor.report(
+            reports.ReportItem.error(
+                reports.messages.ResourceRestartNodeIsForMultiinstanceOnly(
+                    str(resource_el.attrib["id"]),
+                    resource_el.tag,
+                    node,
+                )
+            )
+        )
+
+    if timeout is not None:
+        env.report_processor.report_list(
+            ValueTimeInterval("timeout").validate({"timeout": timeout})
+        )
+
+    if env.report_processor.has_errors:
+        raise LibraryError()
+
+    resource_restart(
+        env.cmd_runner(),
+        str(resource_el.attrib["id"]),
+        node=node,
+        timeout=timeout,
     )

pcs_test/tier0/lib/commands/resource/test_restart.py
@@ -1,0 +1,177 @@
+from unittest import TestCase
+
+from pcs.common import reports
+from pcs.lib.commands import resource
+
+from pcs_test.tools import fixture
+from pcs_test.tools.command_env import get_env_tools
+
+
+class ResourceRestart(TestCase):
+    def setUp(self):
+        self.env_assist, self.config = get_env_tools(test_case=self)
+        resources_xml = """
+            <resources>
+                <primitive id="S1" class="stonith" />
+                <primitive id="R1" class="ocf">
+                    <meta_attributes id="R1-meta" />
+                </primitive>
+                <bundle id="B1">
+                    <primitive id="B1R1" />
+                </bundle>
+                <clone id="C1">
+                    <primitive id="C1R1" />
+                </clone>
+                <clone id="C2">
+                    <group id="C2G1">
+                        <primitive id="C2R1" />
+                    </group>
+                </clone>
+            </resources>
+        """
+        self.config.runner.cib.load(resources=resources_xml)
+
+    def test_success(self):
+        self.config.runner.pcmk.resource_restart("R1")
+        resource.restart(self.env_assist.get_env(), "R1")
+
+    def test_success_bundle_member(self):
+        self.config.runner.pcmk.resource_restart("B1")
+        resource.restart(self.env_assist.get_env(), "B1R1")
+        self.env_assist.assert_reports(
+            [
+                fixture.warn(
+                    reports.codes.RESOURCE_RESTART_USING_PARENT_RESOURCE,
+                    resource="B1R1",
+                    parent="B1",
+                )
+            ]
+        )
+
+    def test_success_clone_member(self):
+        self.config.runner.pcmk.resource_restart("C1")
+        resource.restart(self.env_assist.get_env(), "C1R1")
+        self.env_assist.assert_reports(
+            [
+                fixture.warn(
+                    reports.codes.RESOURCE_RESTART_USING_PARENT_RESOURCE,
+                    resource="C1R1",
+                    parent="C1",
+                )
+            ]
+        )
+
+    def test_success_clone_group_member(self):
+        self.config.runner.pcmk.resource_restart("C2")
+        resource.restart(self.env_assist.get_env(), "C2R1")
+        self.env_assist.assert_reports(
+            [
+                fixture.warn(
+                    reports.codes.RESOURCE_RESTART_USING_PARENT_RESOURCE,
+                    resource="C2R1",
+                    parent="C2",
+                )
+            ]
+        )
+
+    def test_success_timeout(self):
+        self.config.runner.pcmk.resource_restart("R1", timeout="10")
+        resource.restart(self.env_assist.get_env(), "R1", timeout="10")
+
+    def test_success_all_options(self):
+        self.config.runner.pcmk.resource_restart(
+            "C1", node="node1", timeout="10"
+        )
+        resource.restart(self.env_assist.get_env(), "C1", "node1", "10")
+
+    def test_bad_timeout(self):
+        self.env_assist.assert_raise_library_error(
+            lambda: resource.restart(
+                self.env_assist.get_env(), "R1", timeout="a while"
+            )
+        )
+        self.env_assist.assert_reports(
+            [
+                fixture.error(
+                    reports.codes.INVALID_OPTION_VALUE,
+                    option_name="timeout",
+                    option_value="a while",
+                    allowed_values="time interval (e.g. 1, 2s, 3m, 4h, ...)",
+                    cannot_be_empty=False,
+                    forbidden_characters=None,
+                )
+            ]
+        )
+
+    def test_resource_not_found(self):
+        self.env_assist.assert_raise_library_error(
+            lambda: resource.restart(self.env_assist.get_env(), "RX")
+        )
+        self.env_assist.assert_reports(
+            [
+                fixture.error(
+                    reports.codes.ID_NOT_FOUND,
+                    id="RX",
+                    expected_types=["resource"],
+                    context_type="",
+                    context_id="",
+                )
+            ]
+        )
+
+    def test_not_a_resource(self):
+        self.env_assist.assert_raise_library_error(
+            lambda: resource.restart(self.env_assist.get_env(), "R1-meta")
+        )
+        self.env_assist.assert_reports(
+            [
+                fixture.error(
+                    reports.codes.ID_BELONGS_TO_UNEXPECTED_TYPE,
+                    id="R1-meta",
+                    expected_types=["resource"],
+                    current_type="meta_attributes",
+                )
+            ]
+        )
+
+    def test_stonith(self):
+        self.config.runner.pcmk.resource_restart("S1")
+        resource.restart(self.env_assist.get_env(), "S1")
+
+    def test_node_not_multitinstance(self):
+        self.env_assist.assert_raise_library_error(
+            lambda: resource.restart(
+                self.env_assist.get_env(), "R1", node="node1"
+            )
+        )
+        self.env_assist.assert_reports(
+            [
+                fixture.error(
+                    reports.codes.RESOURCE_RESTART_NODE_IS_FOR_MULTIINSTANCE_ONLY,
+                    resource="R1",
+                    resource_type="primitive",
+                    node="node1",
+                )
+            ]
+        )
+
+    def test_node_multitinstance(self):
+        self.config.runner.pcmk.resource_restart("C1", node="node1")
+        resource.restart(self.env_assist.get_env(), "C1", "node1")
+
+    def test_restart_error(self):
+        self.config.runner.pcmk.resource_restart(
+            "R1", stdout="some output", stderr="some error", returncode=1
+        )
+        self.env_assist.assert_raise_library_error(
+            lambda: resource.restart(self.env_assist.get_env(), "R1"),
+            [
+                fixture.error(
+                    reports.codes.RESOURCE_RESTART_ERROR,
+                    reason="some error\nsome output",
+                    resource="R1",
+                    node=None,
+                )
+            ],
+            expected_in_processor=False,
+        )

pcs/cli/routing/resource.py
@@ -50,7 +50,7 @@ resource_cmd = create_router(
         "enable": resource.resource_enable_cmd,
         "disable": resource.resource_disable_cmd,
         "safe-disable": resource.resource_safe_disable_cmd,
-        "restart": resource.resource_restart,
+        "restart": resource.resource_restart_cmd,
         "debug-start": partial(
             resource.resource_force_action, action="debug-start"
         ),

pcs/resource.py
@@ -2793,56 +2793,30 @@ def resource_disable(argv: Argv) -> Optional[bool]:
     return None
 
 
-def resource_restart(lib: Any, argv: Argv, modifiers: InputModifiers) -> None:
+def resource_restart_cmd(
+    lib: Any, argv: Argv, modifiers: InputModifiers
+) -> None:
     """
     Options:
       * --wait
     """
     modifiers.ensure_only_supported("--wait")
     if not argv:
-        utils.err("You must specify a resource to restart")
-    _check_is_not_stonith(lib, [argv[0]])
-
-    dom = utils.get_cib_dom()
-    node = None
-    resource = argv.pop(0)
-
-    real_res = utils.dom_get_resource_clone_ms_parent(
-        dom, resource
-    ) or utils.dom_get_resource_bundle_parent(dom, resource)
-    if real_res:
-        warn(
-            (
-                "using {resource_id}... (if a resource is a clone or bundle "
-                "you must use the clone or bundle name)"
-            ).format(resource_id=real_res.getAttribute("id"))
+        raise CmdLineInputError(
+            "You must specify a resource to restart",
+            show_both_usage_and_message=True,
         )
-        resource = real_res.getAttribute("id")
-
-    args = ["crm_resource", "--restart", "--resource", resource]
+    resource = argv.pop(0)
+    node = argv.pop(0) if argv else None
     if argv:
-        node = argv.pop(0)
-        if not (
-            utils.dom_get_clone(dom, resource)
-            or utils.dom_get_master(dom, resource)
-            or utils.dom_get_bundle(dom, resource)
-        ):
-            utils.err(
-                "can only restart on a specific node for a clone or bundle "
-                "resource"
-            )
-        args.extend(["--node", node])
+        raise CmdLineInputError()
 
-    if modifiers.is_specified("--wait"):
-        wait = modifiers.get("--wait")
-        if wait:
-            args.extend(["--timeout", str(wait)])
-        else:
-            utils.err("You must specify the number of seconds to wait")
+    _check_is_not_stonith(lib, [resource])
+    timeout = (
+        modifiers.get("--wait") if modifiers.is_specified("--wait") else None
+    )
 
-    output, retval = utils.run(args)
-    if retval != 0:
-        utils.err(output)
+    lib.resource.restart(resource, node, timeout)
 
     print_to_stderr(f"{resource} successfully restarted")
 

pcs/cli/common/lib_wrapper.py
@@ -405,15 +405,16 @@ def load_module(env, middleware_factory, name):
                 "enable": resource.enable,
                 "get_configured_resources": resource.get_configured_resources,
                 "get_failcounts": resource.get_failcounts,
+                "get_resource_relations_tree": (
+                    resource.get_resource_relations_tree
+                ),
                 "group_add": resource.group_add,
                 "is_any_resource_except_stonith": resource.is_any_resource_except_stonith,
                 "is_any_stonith": resource.is_any_stonith,
                 "manage": resource.manage,
                 "move": resource.move,
                 "move_autoclean": resource.move_autoclean,
-                "get_resource_relations_tree": (
-                    resource.get_resource_relations_tree
-                ),
+                "restart": resource.restart,
                 "unmanage": resource.unmanage,
                 "unmove_unban": resource.unmove_unban,
             },

pcs/cli/routing/booth.py
@@ -6,7 +6,7 @@ from pcs.cli.booth import command
 from pcs.cli.common.routing import create_router
 from pcs.resource import (
     resource_remove,
-    resource_restart,
+    resource_restart_cmd,
 )
 
 mapping = {
@@ -30,7 +30,7 @@ mapping = {
     # a function to pcs.lib
     "delete": command.get_remove_from_cluster(resource_remove),  # type:ignore
     "remove": command.get_remove_from_cluster(resource_remove),  # type:ignore
-    "restart": command.get_restart(resource_restart),  # type:ignore
+    "restart": command.get_restart(resource_restart_cmd),  # type:ignore
     "sync": command.sync,
     "pull": command.pull,
     "enable": command.enable,

pcs/common/reports/codes.py
@@ -446,6 +446,13 @@ RESOURCE_OPERATION_INTERVAL_DUPLICATION = M(
 )
 RESOURCE_REFRESH_ERROR = M("RESOURCE_REFRESH_ERROR")
 RESOURCE_REFRESH_TOO_TIME_CONSUMING = M("RESOURCE_REFRESH_TOO_TIME_CONSUMING")
+RESOURCE_RESTART_ERROR = M("RESOURCE_RESTART_ERROR")
+RESOURCE_RESTART_NODE_IS_FOR_MULTIINSTANCE_ONLY = M(
+    "RESOURCE_RESTART_NODE_IS_FOR_MULTIINSTANCE_ONLY"
+)
+RESOURCE_RESTART_USING_PARENT_RESOURCE = M(
+    "RESOURCE_RESTART_USING_PARENT_RESOURCE"
+)
 RESOURCE_RUNNING_ON_NODES = M("RESOURCE_RUNNING_ON_NODES")
 RESOURCE_UNMOVE_UNBAN_PCMK_ERROR = M("RESOURCE_UNMOVE_UNBAN_PCMK_ERROR")
 RESOURCE_UNMOVE_UNBAN_PCMK_SUCCESS = M("RESOURCE_UNMOVE_UNBAN_PCMK_SUCCESS")

pcs/common/reports/messages.py
@@ -3381,6 +3381,72 @@ class WaitForIdleNotLiveCluster(ReportItemMessage):
 
 
 @dataclass(frozen=True)
+class ResourceRestartError(ReportItemMessage):
+    """
+    An error occurred when restarting a resource in pacemaker
+
+    reason -- error description
+    resource -- resource which has been restarted
+    node -- node where the resource has been restarted
+    """
+
+    reason: str
+    resource: str
+    node: Optional[str] = None
+    _code = codes.RESOURCE_RESTART_ERROR
+
+    @property
+    def message(self) -> str:
+        return f"Unable to restart resource '{self.resource}':\n{self.reason}"
+
+
+@dataclass(frozen=True)
+class ResourceRestartNodeIsForMultiinstanceOnly(ReportItemMessage):
+    """
+    Restart can be limited to a specified node only for multiinstance resources
+
+    resource -- resource to be restarted
+    resource_type -- actual type of the resource
+    node -- node where the resource was to be restarted
+    """
+
+    resource: str
+    resource_type: str
+    node: str
+    _code = codes.RESOURCE_RESTART_NODE_IS_FOR_MULTIINSTANCE_ONLY
+
+    @property
+    def message(self) -> str:
+        resource_type = _type_to_string(self.resource_type, article=True)
+        return (
+            "Can only restart on a specific node for a clone or bundle, "
+            f"'{self.resource}' is {resource_type}"
+        )
+
+
+@dataclass(frozen=True)
+class ResourceRestartUsingParentRersource(ReportItemMessage):
+    """
+    Multiinstance parent is restarted instead of a specified primitive
+
+    resource -- resource which has been asked to be restarted
+    parent -- parent resource to be restarted instead
+    """
+
+    resource: str
+    parent: str
+    _code = codes.RESOURCE_RESTART_USING_PARENT_RESOURCE
+
+    @property
+    def message(self) -> str:
+        return (
+            f"Restarting '{self.parent}' instead...\n"
+            "(If a resource is a clone or bundle, you must use the clone or "
+            "bundle instead)"
+        )
+
+
+@dataclass(frozen=True)
 class ResourceCleanupError(ReportItemMessage):
     """
     An error occurred when deleting resource failed operations in pacemaker

commit 437a441c94103395f31fca841c1cf94a5c3c5cf0

fix restarting bundle instances
* fixes a regression introduced in ccffba735128ea4be62aa33e7319114d8b26a8b0

pcs/lib/commands/resource.py
@@ -2564,56 +2564,59 @@ def restart(
     timeout -- abort if the command doesn't finish in this time (integer + unit)
     """
     cib = env.get_cib()
+
+    # To be able to restart bundle instances, which are not to be found in CIB,
+    # do not fail if specified ID is not found in CIB. Pacemaker provides
+    # reasonable messages when the ID to be restarted is not a resource or
+    # doesn't exist. We only search for the resource in order to provide hints
+    # when the user attempts to restart bundle's or clone's inner resources.
+    resource_found = False
     try:
         resource_el = get_element_by_id(cib, resource_id)
-    except ElementNotFound as e:
-        env.report_processor.report(
-            ReportItem.error(
-                reports.messages.IdNotFound(
-                    resource_id, expected_types=["resource"]
-                )
-            )
-        )
-        raise LibraryError() from e
-    if not resource.common.is_resource(resource_el):
-        env.report_processor.report(
-            ReportItem.error(
-                reports.messages.IdBelongsToUnexpectedType(
-                    resource_id,
-                    expected_types=["resource"],
-                    current_type=resource_el.tag,
-                )
-            )
-        )
-        raise LibraryError()
+        resource_found = True
+    except ElementNotFound:
+        pass
 
-    parent_resource_el = resource.clone.get_parent_any_clone(resource_el)
-    if parent_resource_el is None:
-        parent_resource_el = resource.bundle.get_parent_bundle(resource_el)
-    if parent_resource_el is not None:
-        env.report_processor.report(
-            reports.ReportItem.warning(
-                reports.messages.ResourceRestartUsingParentRersource(
-                    str(resource_el.attrib["id"]),
-                    str(parent_resource_el.attrib["id"]),
+    if resource_found:
+        if not resource.common.is_resource(resource_el):
+            env.report_processor.report(
+                ReportItem.error(
+                    reports.messages.IdBelongsToUnexpectedType(
+                        resource_id,
+                        expected_types=["resource"],
+                        current_type=resource_el.tag,
+                    )
                 )
             )
-        )
-        resource_el = parent_resource_el
+            raise LibraryError()
 
-    if node and not (
-        resource.clone.is_any_clone(resource_el)
-        or resource.bundle.is_bundle(resource_el)
-    ):
-        env.report_processor.report(
-            reports.ReportItem.error(
-                reports.messages.ResourceRestartNodeIsForMultiinstanceOnly(
-                    str(resource_el.attrib["id"]),
-                    resource_el.tag,
-                    node,
+        parent_resource_el = resource.clone.get_parent_any_clone(resource_el)
+        if parent_resource_el is None:
+            parent_resource_el = resource.bundle.get_parent_bundle(resource_el)
+        if parent_resource_el is not None:
+            env.report_processor.report(
+                reports.ReportItem.warning(
+                    reports.messages.ResourceRestartUsingParentRersource(
+                        str(resource_el.attrib["id"]),
+                        str(parent_resource_el.attrib["id"]),
+                    )
                 )
             )
-        )
+            resource_el = parent_resource_el
+
+        if node and not (
+            resource.clone.is_any_clone(resource_el)
+            or resource.bundle.is_bundle(resource_el)
+        ):
+            env.report_processor.report(
+                reports.ReportItem.error(
+                    reports.messages.ResourceRestartNodeIsForMultiinstanceOnly(
+                        str(resource_el.attrib["id"]),
+                        resource_el.tag,
+                        node,
+                    )
+                )
+            )
 
     if timeout is not None:
         env.report_processor.report_list(
@@ -2625,7 +2628,7 @@ def restart(
 
     resource_restart(
         env.cmd_runner(),
-        str(resource_el.attrib["id"]),
+        str(resource_el.attrib["id"]) if resource_found else resource_id,
         node=node,
         timeout=timeout,
     )

pcs_test/tier0/lib/commands/resource/test_restart.py
@@ -104,20 +104,8 @@ class ResourceRestart(TestCase):
         )
 
     def test_resource_not_found(self):
-        self.env_assist.assert_raise_library_error(
-            lambda: resource.restart(self.env_assist.get_env(), "RX")
-        )
-        self.env_assist.assert_reports(
-            [
-                fixture.error(
-                    reports.codes.ID_NOT_FOUND,
-                    id="RX",
-                    expected_types=["resource"],
-                    context_type="",
-                    context_id="",
-                )
-            ]
-        )
+        self.config.runner.pcmk.resource_restart("RX")
+        resource.restart(self.env_assist.get_env(), "RX")
 
     def test_not_a_resource(self):
         self.env_assist.assert_raise_library_error(