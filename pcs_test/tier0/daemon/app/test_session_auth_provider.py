9768d0d45b319139efa88cef32ff141ee28e58a1
Matej Slivka <mslivka@redhat.com>
2026-07-21 10:54:06 +0200

    Modify get_cookie to not use default argument

--- pcs_test/tier0/daemon/app/test_session_auth_provider.py
+++ pcs_test/tier0/daemon/app/test_session_auth_provider.py
@@ -35,9 +35,7 @@ class SessionAuthProviderTest(IsolatedAsyncioTestCase):
     def setUp(self):
         self.handler = mock.Mock()
         cookie_jar = {webui.auth_provider.PCSD_SESSION: "session123"}
-        self.handler.get_cookie.side_effect = (
-            lambda name, default: cookie_jar.get(name, default)  # noqa: PLW0108
-        )
+        self.handler.get_cookie.side_effect = cookie_jar.get
 
         self.session_storage = MockStorage()
 
